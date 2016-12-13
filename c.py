ff0e1917f978a6ee360bf41743232d0875705904
David Cottrell <cottrell@users.noreply.github.com>
2016-12-13 22:21:26 +0000

    junk

--- c.py
+++ c.py
@@ -1,85 +1,58 @@
-"""
-@CachableComputationFactory
-def f(a, b=None):
-    pass
-
-f(1.2).compute()
-f(1.2).compute(force=True)
-"""
 from functools import wraps
 import inspect
-
-class Counter():
-    def __init__(self):
-        self._current = 0
-        self._min = 0
-        self._max = 0
-    def get(self):
-        return self._current
-    def __call__(self):
-        return self._current
-    def go(self, x):
-        self._current = x
-
-version = Counter()
+import decorator
+import logging
+logging.getLogger().setLevel(logging.INFO)
 
 class VersionedCachedComputation():
     def __init__(self, fun, *args, **kwargs):
-        print('HEREHERE', kwargs)
-        # TODO check argspec of fun and provided args, kwargs on init
         self._args = args
         self._versioned_args = kwargs.get('v_args', None)
         self._kwargs = kwargs
         if 'v_args' in kwargs:
             self._kwargs.pop('v_args')
         self._fun = fun
-        self._name = fun.__name__
-        print('{} versioned args: {}'.format(self._name, self._versioned_args))
     def compute(self):
-        print("I am {} and I am running with args {}".format(self._name, (self._args, self._kwargs)))
+        logging.info("VCC args={}, kwargs={}".format(self._args, self._kwargs))
         if self._versioned_args is not None:
-            print("HERE")
             v_args = [x.compute() for x in self._versioned_args]
             return self._fun(*self._args, v_args=v_args, **self._kwargs)
         else:
             return self._fun(*self._args, **self._kwargs)
-    def _get_args(self):
-        args = self._args
-        kwargs = self._kwargs
-        if self._versioned_args is not None:
-            kwargs['v_args'] = [x._get_args() for x in self._versioned_args]
-        return args, tuple(kwargs.items())
 
+# using decorator.decorate allows you to avoid closures
+def _versioned_cached_computation_factory(fun, *args, **kwargs):
+    return VersionedCachedComputation(fun, *args, *kwargs)
 
-class VersionedCachedComputationFactory(object):
-    def __init__(self, name=None, **kwargs):
-        self.name = name
-        if self.name == None:
-            self.name = 'unnamed' # how to get name from caller module
-    def __call__(self, fun):
-        # TODO need to get kwarg defaults ... wraps is not picking them  up
-        print('{} computation construction from {}'.format(self.name, fun.__name__))
-        @wraps(fun)
-        def inner(*args, **kwargs):
-            return VersionedCachedComputation(fun, *args, **kwargs)
-        return inner
+def versioned_cached_computation_factory(fun):
+    return decorator.decorate(fun, _return_vcc)
 
-
-VCCF = VersionedCachedComputationFactory(name='main vccf')
-
-@VCCF
-def A(x, y=None):
+@versioned_cached_computation_factory
+def f(x, y='here'):
     print("me I'm running x={}, y={}".format(x, y))
     return str(x) + ":" + str(y)
 
-def b(x, y=None, v_args=[A(43.3, 23)]):
+@versioned_cached_computation_factory
+def A(x, y='here'):
     print("me I'm running x={}, y={}".format(x, y))
+    return str(x) + ":" + str(y)
+
+@versioned_cached_computation_factory
+def B(x, y='more', v_args=[A(43.3, 23)]):
+    print("me I'm running x={}, y={}, v_args={}".format(x, y, v_args))
     return str(x) + ":" + str(y) + ':' + str(v_args[0])
 
-B = VCCF(b)
+# NOPE
 
-# these need to be the same
-print(inspect.getfullargspec(b))
-print(inspect.getfullargspec(B(12, 2).compute))
-
-
+# class VersionedCachedComputationFactory(object):
+#     def __init__(self, fun, *args, **kwargs):
+#         self._fun = fun
+#         self._name = 'needname'
+#         argspec = inspect.getfullargspec(fun)
+#         self._kwarg_defaults = dict(zip(argspec.args[::-1], argspec.defaults[::-1]))
+#         logging.info("VCCF.__init__:{} _kwarg_defaults={}".format(self._name, self._kwarg_defaults))
+#     def __call__(self, *args, **kwargs):
+#         kwargs_out = dict(self._kwarg_defaults)
+#         kwargs_out.update(kwargs)
+#         logging.info("VCCF.__call__:{} args={}, kwargs={}".format(self._name, args, kwargs_out))
+#         return VersionedCachedComputation(self._fun, *args, **kwargs_out)
