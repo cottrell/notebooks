898cd9e23bc68bbd71eed2aebfb5dc6a9b22bf1c
David Cottrell <cottrell@users.noreply.github.com>
2017-01-05 20:39:09 +0000

    update

--- fx/a.py
+++ fx/a.py
@@ -6,14 +6,15 @@ _path = os.path.dirname(os.path.abspath(__name__))
 site.addsitedir(_path)
 import datastore.sources as ds
 
-# YTYPE = 'linear'
-YTYPE = 'sign'
-ndays_pred = 30 # working days in the future, 20 is a month?
+YTYPE = 'linear'
+# YTYPE = 'sign'
+ndays_pred = 40 # working days in the future, 20 is a month?
 # CCYCCY = 'GBPUSD'
 # CCYCCY = 'CADUSD'
 # CCYCCY = 'CHFUSD'
 CCYCCY = 'CNYUSD' # interesting
-n_features = 4
+n_features = 12 
+n_history = 1
 
 def _get_data():
     df = ds.quandl.CURRFX.get_all_cleaned()
@@ -21,7 +22,6 @@ def _get_data():
 
 def _get_shift_features(X):
     X = X.copy()
-    n_history = 1
     # create shifts as clumsy way of getting last few days as features
     temp = list()
     for i in range(n_history):
@@ -90,13 +90,13 @@ if YTYPE == 'linear':
     etr = se.ExtraTreesRegressor(bootstrap=True, criterion='mse', max_depth=3,
            max_features='auto', max_leaf_nodes=None, min_samples_leaf=5, min_samples_split=2, min_weight_fraction_leaf=0.0,
            n_estimators=100, n_jobs=1, oob_score=True, random_state=None, verbose=0, warm_start=False)
-    etr.fit(X_fit.fillna(method='ffill'), y_fit)
+    etr.fit(X_fit.fillna(method='ffill').fillna(0), y_fit)
 elif YTYPE == 'sign':
     etr = se.ExtraTreesClassifier(bootstrap=True, class_weight=None, criterion='gini', max_depth=3,
             max_features='auto', max_leaf_nodes=None, min_samples_leaf=1, min_samples_split=2,
             min_weight_fraction_leaf=0.0, n_estimators=100, n_jobs=-1, oob_score=True, random_state=None,
             verbose=1, warm_start=False)
-    etr.fit(X_fit.fillna(method='ffill'), y_fit)
+    etr.fit(X_fit.fillna(method='ffill').fillna(0), y_fit)
 
 s = pd.Series(etr.feature_importances_, index=X_fit.columns)
 s.sort(ascending=True)
@@ -108,6 +108,7 @@ def doplotA(y, y_pred):
     plot(y[ind_test], y_pred[ind_test], 'r.', alpha=0.5)
 
 from pylab import *
+ion()
 figure(3)
 clf()
 ax = subplot(122)
@@ -115,48 +116,47 @@ s.head(n=20).plot(ax=ax, kind='barh')
 figure(4)
 clf()
 if YTYPE == 'linear':
-    y_pred = etr.predict(X.fillna(method='ffill')) # TODO handle nan
+    y_pred = etr.predict(X.fillna(method='ffill').fillna(0)) # TODO handle nan
     doplotA(y, y_pred)
     title('Extra Trees Pred for feature selection')
 
-# really need to get this in a proper pipeline
-i_features = np.argsort(etr.feature_importances_) < n_features
-reduce_features = lambda x: x.iloc[:,i_features]
-X_fit = reduce_features(X_fit)
-X_test = reduce_features(X_test)
-X_train = reduce_features(X_train)
-X_eval = reduce_features(X_eval)
-X = reduce_features(X)
-
-import xgboost as xgb
-if YTYPE == 'linear':
-    model = xgb.XGBRegressor(base_score=0.5, colsample_bylevel=1, colsample_bytree=0.5, gamma=0, learning_rate=0.1,
-            max_delta_step=0, max_depth=3, min_child_weight=1, missing=None, n_estimators=100, nthread=-1,
-            objective='reg:linear', reg_alpha=0, reg_lambda=1, seed=2, silent=True, subsample=0.3)
-    model.fit(X_fit, y_fit, eval_set=[(X_fit, y_fit), (X_eval, y_eval), (X_test, y_test)])
-elif YTYPE == 'sign':
-    model = xgb.XGBClassifier(base_score=0.5, colsample_bylevel=1, colsample_bytree=0.5, gamma=0, learning_rate=0.01,
-            max_delta_step=0, max_depth=3, min_child_weight=1, missing=None, n_estimators=1000, nthread=-1,
-            reg_alpha=0, reg_lambda=1, scale_pos_weight=1, seed=0, silent=True, subsample=1)
-    model.fit(X_fit, y_fit, eval_set=[(X_fit, y_fit), (X_eval, y_eval), (X_test, y_test)], eval_metric='auc')
-
-y_pred = model.predict(X)
-
-ion()
-figure(1)
-clf()
-if YTYPE == 'linear':
-    doplotA(y, y_pred)
-    title('y vs y_pred {} (ndays = {})'.format(y_colname, ndays_pred))
-
-    df_allfeatures['y_pred'] = y_pred
-    df_allfeatures['y'] = y
-figure(2)
-clf()
-if YTYPE == 'linear':
-    ax = subplot(111)
-    df_allfeatures.iloc[ind_test][['y', 'y_pred']].plot(ax=ax)
-    title('y vs y_pred on test set\n{} (ndays = {})'.format(y_colname, ndays_pred))
-
-
-
+# # really need to get this in a proper pipeline
+# i_features = np.argsort(etr.feature_importances_) < n_features
+# reduce_features = lambda x: x.iloc[:,i_features]
+# X_fit = reduce_features(X_fit)
+# X_test = reduce_features(X_test)
+# X_train = reduce_features(X_train)
+# X_eval = reduce_features(X_eval)
+# X = reduce_features(X)
+# 
+# import xgboost as xgb
+# if YTYPE == 'linear':
+#     model = xgb.XGBRegressor(base_score=0.5, colsample_bylevel=1, colsample_bytree=0.5, gamma=0, learning_rate=0.1,
+#             max_delta_step=0, max_depth=3, min_child_weight=1, missing=None, n_estimators=100, nthread=-1,
+#             objective='reg:linear', reg_alpha=0, reg_lambda=1, seed=2, silent=True, subsample=0.3)
+#     model.fit(X_fit, y_fit, eval_set=[(X_fit, y_fit), (X_eval, y_eval), (X_test, y_test)])
+# elif YTYPE == 'sign':
+#     model = xgb.XGBClassifier(base_score=0.5, colsample_bylevel=1, colsample_bytree=0.5, gamma=0, learning_rate=0.01,
+#             max_delta_step=0, max_depth=3, min_child_weight=1, missing=None, n_estimators=1000, nthread=-1,
+#             reg_alpha=0, reg_lambda=1, scale_pos_weight=1, seed=0, silent=True, subsample=1)
+#     model.fit(X_fit, y_fit, eval_set=[(X_fit, y_fit), (X_eval, y_eval), (X_test, y_test)], eval_metric='auc')
+# 
+# y_pred = model.predict(X)
+# 
+# figure(1)
+# clf()
+# if YTYPE == 'linear':
+#     doplotA(y, y_pred)
+#     title('y vs y_pred {} (ndays = {})'.format(y_colname, ndays_pred))
+# 
+#     df_allfeatures['y_pred'] = y_pred
+#     df_allfeatures['y'] = y
+# figure(2)
+# clf()
+# if YTYPE == 'linear':
+#     ax = subplot(111)
+#     df_allfeatures.iloc[ind_test][['y', 'y_pred']].plot(ax=ax)
+#     title('y vs y_pred on test set\n{} (ndays = {})'.format(y_colname, ndays_pred))
+# 
+# 
+# 
