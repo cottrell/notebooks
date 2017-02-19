e299457632f8a69711cbbfd2eaf69bd68677b36d
David Cottrell <cottrell@users.noreply.github.com>
2017-02-19 00:50:37 +0000

    as package

---
+++ mylib/mylib/tools.py
@@ -0,0 +1,32 @@
+import logging
+import time
+import pandas as pd
+import threading
+
+class TimeLogger():
+
+    def __init__(self, log=logging.warning):
+        self.d = dict()
+        self.log = log
+
+    @contextlib.contextmanager
+    def timedlogger(self, *name):
+        start = time.time()
+        # self.log("%s ..." % (name,))
+        yield
+        end = time.time()
+        interval = end - start
+        self.log("%f s : %s " % (interval, '-'.join(name)))
+        self.d[name] = [start, end, interval]
+
+    def get_frame(self):
+        return pd.DataFrame(self.d, index=['start', 'stop', 'ellapsed']).T
+
+    def clear(self):
+        self.d = dict()
+
+class AttrDict(dict):
+
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
---
+++ mylib/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mylib',
+      version='0.1',
+      description='todo',
+      packages=['mylib'],
+      zip_safe=False)
