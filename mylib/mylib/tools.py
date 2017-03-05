e01c8c1724120d096f4f2c28758aae0aa828e316
David Cottrell <cottrell@users.noreply.github.com>
2017-03-05 23:15:23 +0000

    add some tools

--- mylib/mylib/tools.py
+++ mylib/mylib/tools.py
@@ -3,7 +3,61 @@ import time
 import pandas as pd
 import threading
 import contextlib
+import asyncio
 
+def dict_of_lists(d):
+    r = dict()
+    for k, v in d.items():
+        if v not in r:
+            r[v] = list()
+        r[v].append(k)
+    r = {k: sorted(v) for k, v in r.items()}
+    return v
+
+def schedule_coroutine(target, *, loop=None):
+    """Schedules target coroutine in the given event loop
+
+    If not given, *loop* defaults to the current thread's event loop
+
+    Returns the scheduled task.
+    """
+    if asyncio.iscoroutine(target):
+        return asyncio.ensure_future(target, loop=loop)
+    raise TypeError("target must be a coroutine, not {!r}".format(type(target)))
+
+def run_in_foreground(tasks, loop=None):
+    """Runs event loop in current thread until the given task completes
+
+    Returns the result of the task.
+    For more complex conditions, combine with asyncio.wait()
+    To include a timeout, combine with asyncio.wait_for()
+    """
+    if loop is None:
+        loop = asyncio.get_event_loop()
+    tasks = [asyncio.ensure_future(task, loop=loop) for task in tasks]
+    tasks = asyncio.gather(tasks)
+    try:
+        res = loop.run_until_complete(tasks)
+    except KeyboardInterrupt as e:
+        print("KeyboardInterrupt: cancelling tasks ...")
+        tasks.cancel()
+        loop.run_forever()
+        tasks.exception()
+    finally:
+        loop.close()
+
+@contextlib.contextmanager
+def spark_manager(spark_master=None, name='default name'):
+    if spark_master is None:
+        spark_master = os.environ['SPARK_MASTER']
+    conf = SparkConf().setMaster(spark_master) \
+                      .setAppName(name) \
+                      .set("spark.executor.memory", '1000m')
+    spark_context = SparkContext(conf=conf)
+    try:
+        yield spark_context
+    finally:
+        spark_context.stop()
 
 class TimeLogger():
 
---
+++ sparkstreamingexample/do.py
@@ -0,0 +1,38 @@
+import asyncio
+import os
+import pandas as pd
+import mylib.data # setup.py in notebooks
+from numpy.random import randn
+import string
+
+def gen_data(m=10, n=4):
+    df = pd.DataFrame(randn(m, n + 1))
+    df.columns = list(string.ascii_lowercase[:n + 1])
+    df['a'] = mylib.data.id_generator(m)
+    df = df.set_index('a')
+    return df
+
+async def periodically_generate_new_data(period=5, nmax=100, basedir='./data'):
+    if not os.path.exists(basedir):
+        os.makedirs(basedir)
+    dt = 1
+    if nmax is None:
+        dt = 0
+    t = 0
+    file_counter = 0
+    while t < nmax:
+        df = gen_data()
+        filename = os.path.join(basedir, '{}.csv.gz'.format(file_counter))
+        file_counter += 1
+        t += dt
+        print('asdf {} {}'.format(t, filename))
+        df.to_csv(filename, compression='gzip')
+        await asyncio.sleep(period)
+
+import itertools
+async def ticker():
+    for i in itertools.count():
+        await asyncio.sleep(1)
+
+
+
