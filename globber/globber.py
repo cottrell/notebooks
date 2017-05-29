427e31d929524c99c1b766634c80635757aa1963
David Cottrell <cottrell@users.noreply.github.com>
2017-05-29 22:53:14 +0100

    asdf

--- globber/globber.py
+++ globber/globber.py
@@ -12,4 +12,24 @@ def globs_with_timestamps(*patterns):
         out.extend(files)
     return out
 
+import simplekv
+import simplekv.fs
+dbname = 'here.db'
+store = simplekv.fs.FilesystemStore(dbname)
 
+class GlobberRunner():
+    def __init__(self, name, gen_patterns, version=None):
+        self.name = name # TODO use inspect get from code
+        self.gen_patterns = gen_patterns
+        self.version = str(version) if version is not None else '0'
+    def run_once(self, *args, **kwargs):
+        patterns = self.gen_patterns(*args, **kwargs)
+        if type(patterns) in {str, bytes}:
+            patterns = [patterns]
+        filenames_with_timestamps = globs_with_timestamps(*patterns)
+        for x in filenames_with_timestamps:
+            key = hash((self.name, self.version, x))
+            value = (self.name, self.version, x)
+            print(key, value)
+
+g = GlobberRunner('test', lambda : 'data/*.csv.gz')
