27de1521d8d6d7287a49c55aa596d1fd475fed11
David Cottrell <cottrell@users.noreply.github.com>
2017-10-08 20:18:17 +0100

    update

--- supervisord/run.bash
+++ supervisord/run.bash
@@ -3,3 +3,4 @@ export DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 echo running supervisord -c $DIR/supervisord.conf
 supervisord -c $DIR/supervisord.conf
 echo 'then run supervisorctl'
+# start thegroup:*
