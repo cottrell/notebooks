777e5efef6df4021c6452c8d8bf3a92438fcab66
David Cottrell <cottrell@users.noreply.github.com>
2017-10-27 10:08:50 -0300

    update

--- api/flask_apispec/example.py
+++ api/flask_apispec/example.py
@@ -3,6 +3,10 @@ from webargs import fields
 from flask_apispec import use_kwargs, marshal_with
 from apispec import APISpec
 from flask_apispec.extension import FlaskApiSpec
+from flask_apispec import ResourceMeta, Ref, doc, marshal_with, use_kwargs
+import six
+from flask import make_response
+from flask_apispec.views import MethodResource
 
 app = flask.Flask(__name__)
 
@@ -15,15 +19,42 @@ app.config.update({
     'APISPEC_SWAGGER_URL': '/swagger/',
 })
 
+# simple methods
+
 @app.route('/')
 def welcome(**kwargs):
     return 'api is up! {}'.format(kwargs)
 
 @app.route('/users')
 @use_kwargs({'kinds': fields.Str()})
-def list_pets(**kwargs):
+def list_users(**kwargs):
     return "users {}".format(kwargs)
 
+# resources
+
+class MethodResourceMeta(ResourceMeta, flask.views.MethodViewType):
+    pass
+
+class MethodResource(six.with_metaclass(MethodResourceMeta, flask.views.MethodView)):
+    methods = None
+
+class UserResource(MethodResource):
+
+    def get(self, _id):
+        return 'get'
+
+    @use_kwargs({'_id': fields.Str()})
+    def post(self, **kwargs):
+        return Pet(**kwargs)
+
+    @use_kwargs({'_id': fields.Str()})
+    def put(self, _id, **kwargs):
+        return 'put'
+
+    def delete(self, _id):
+        return make_response('delete', 204)
+
 docs = FlaskApiSpec(app)
-docs.register(list_pets)
+docs.register(list_users)
+docs.register(UserResource) # this fails to be found
 # see http://0.0.0.0:5000/swagger/
--- api/flask_apispec/run.sh
+++ api/flask_apispec/run.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-FLASK_APP=example.py flask run --host=0.0.0.0 --reload --debugger
+
+FLASK_APP=example.py
+if [[ "$1" ]]; then
+    FLASK_APP=$1
+fi
+export FLASK_APP
+flask run --host=0.0.0.0 --reload --debugger
+
