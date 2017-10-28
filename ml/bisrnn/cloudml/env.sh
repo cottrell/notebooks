23ff376030a41bebf82d8ff16b72797458ce18fa
David Cottrell <cottrell@users.noreply.github.com>
2017-10-28 13:53:59 -0300

    py27

--- ml/bisrnn/cloudml/env.sh
+++ ml/bisrnn/cloudml/env.sh
@@ -4,5 +4,7 @@ export BUCKET="misc-data-ml"
 export JOB_NAME="job_$(date +%Y%m%d_%H%M%S)"
 export FILENAME=$DIR/../mangled_data.txt.gz
 export GCS_FILENAME=gs://${BUCKET}/${FILENAME}
-# gcloud is maybe only python2.7???
-source ${HOME}/anaconda3/envs/36/bin/activate 36
+export CLOUDSDK_PYTHON=
+source activate py27
+# gcloud is only python2.7 !!! horrible
+source ${HOME}/anaconda3/envs/36/bin/activate py27
---
+++ ml/bisrnn/cloudml/setup_pyenv.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+conda create -n py27 python=2.7
+pip install -r ./requirements.txt
