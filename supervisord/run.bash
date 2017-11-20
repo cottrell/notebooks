690d9586067f7ec7c839f319f760309035fe1d52
David Cottrell <cottrell@users.noreply.github.com>
2017-11-20 20:06:38 +0000

    trap

--- supervisord/run.bash
+++ supervisord/run.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+trap "kill -- -$$" EXIT
 export DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 echo running supervisord -c $DIR/supervisord.conf
 supervisord -c $DIR/supervisord.conf
