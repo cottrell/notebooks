bb159eb4b8230d3bcda592b5fe5a069df9519e61
David Cottrell <cottrell@users.noreply.github.com>
2017-11-22 22:26:06 +0000

    update

--- data/bis/get_working_papers.bash
+++ data/bis/get_working_papers.bash
@@ -6,11 +6,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 [[ -d "$DIR/pdf" ]] || mkdir -p $DIR/pdf
 [[ -d "$DIR/text" ]] || mkdir -p $DIR/text
 
-for x in $(seq 1 665); do
+for x in $(seq 1 678); do
     pdf=work"$x".pdf
     text=work"$x".text
     echo $pdf $text
-    [[ -e "$DIR/pdf/$pdf" ]] || curl http://www.bis.org/publ/$pdf -o $DIR/pdf/$pdf
+    [[ -e "$DIR/pdf/$pdf" ]] || curl https://www.bis.org/publ/$pdf -o $DIR/pdf/$pdf
+    # TODO should remove text if pdf not exist
     [[ -e "$DIR/text/$text" ]] || pdftotext $DIR/pdf/$pdf -nopgbrk $DIR/text/$text
     # [[ $(( $x % 10 )) -eq 0 ]] && wait
 done
