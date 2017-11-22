d13947371d98531b3824ee827d56d5ae20eecacc
David Cottrell <cottrell@users.noreply.github.com>
2017-11-22 22:37:46 +0000

    update

--- data/bis/makedata.py
+++ data/bis/makedata.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import re
 import glob
 import os
 
@@ -6,11 +7,15 @@ mydir = os.path.dirname(os.path.realpath(__file__))
 def makedata():
     files = glob.glob(os.path.join(mydir, 'text/*.text'))
     fout = open(os.path.join(mydir, 'all.text'), 'w')
+    reg = re.compile('\.{2,}')
     for f in files:
+        print('file {}'.format(f))
         for line in open(f):
-            line = line.strip()
+            # line = line.replace('\n', ' ')
+            # line = reg.sub('', line)
             if len(line) > 10:
                 fout.write(line)
+        fout.write('\n')
 
 if __name__ == '__main__':
     makedata()
