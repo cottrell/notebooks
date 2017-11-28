ef11fccf788b31397b94cab4b3a7b834def5b9dc
David Cottrell <cottrell@users.noreply.github.com>
2017-11-28 23:12:05 +0000

    asdf

--- caches.py
+++ caches.py
@@ -4,67 +4,54 @@ import inspect
 import decorator
 import logging
 from pdb import set_trace
-logging.getLogger().setLevel(logging.INFO)
+from dask.delayed import Delayed
+from dask import delayed
+import pickle
+import hashlib
 
-_version = 0
+# key is good in dask now. the other stuff not yet 
 
-try:
-    _cache
-except NameError as e:
-    _cache = dict()
+class Helper():
+    def __init__(self, version, func, *args, **kwargs):
+        self.version = version
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+        self.delayed = delayed(pure=True)(func)(*args, **kwargs)
+    @property
+    def key(self):
+        return (self.version, self.delayed.key)
+    @property
+    def hashkey(self):
+        return hashlib.sha1(pickle.dumps((self.version, self.delayed.key))).hexdigest()
+    def compute(self):
+        self._value = self.delayed.compute()
+        return self._value
+    def write(self):
+        pass
+    def read(self):
+        pass
 
-class VersionedCachedComputation():
-    def __init__(self, fun, *args, **kwargs):
-        # kwargs is somehow never populated, annoying.
-        argspec = inspect.getfullargspec(fun)
-        kwargs = dict()
-        for i, v in enumerate(argspec.defaults[::-1]):
-            k = argspec.args[-(i+1)] # the defaults are already in here
-            kwargs[k] = v
-        args = args[:-len(kwargs)]
-        self._args = args
-        self._versioned_args = kwargs.get('v_args', None)
-        self._kwargs = kwargs
-        if 'v_args' in kwargs:
-            self._kwargs.pop('v_args')
-        self._fun = fun
-        self._name = '{}:{}'.format(inspect.getabsfile(fun), fun.__name__)
-        # set_trace()
+def __versioned(func, *args, **kwargs):
+    return Helper(func.version, func, *args, **kwargs)
 
-    def _get_key(self):
-        """ function signature key """
-        key = self._name, self._args, frozenset(self._kwargs.items())
-        if self._versioned_args is not None:
-            key += tuple([x._get_key() for x in self._versioned_args])
-        return key
+def versioned(version=None):
+    def _versioned(f):
+        f.version = version
+        return decorator.decorate(f, __versioned)
+    return _versioned
 
-    def compute(self, force=False):
-        global _cache
-        key = self._get_key()
-        if key in _cache:
-            logging.info("retrieve from cache for {}".format(key))
-            res = _cache[key]
-        else:
-            logging.info("compute for {}".format(key))
-            if self._versioned_args is not None:
-                v_args = [x.compute() for x in self._versioned_args]
-                res = self._fun(*self._args, v_args=v_args, **self._kwargs)
-            else:
-                res = self._fun(*self._args, **self._kwargs)
-            _cache[key] = res
-        return res
-
-@decorator.decorator(VersionedCachedComputation)
+@versioned(version=1)
 def A(x, y='here', z='more'):
-    time.sleep(1)
+    print('COMPUTING')
     return str(x) + ":" + str(y)
 
-@decorator.decorator(VersionedCachedComputation)
+@versioned(version=1)
 def B(x, y='more', v_args=[A(41.3, 23)]):
-    time.sleep(1)
+    print('COMPUTING')
     return str(x) + ":" + str(y) + ':' + str(v_args[0])
 
-@decorator.decorator(VersionedCachedComputation)
-def C(x, y='more', v_args=[B(1111), A(41.3, 23)]):
-    time.sleep(1)
-    return str(x) + ":" + str(y) + ':' + str(v_args[0])
+# @delayed(pure=True)
+# def C(x, y='more', v_args=[B(1111), A(41.3, 23)]):
+#     print('COMPUTING')
+#     return str(x) + ":" + str(y) + ':' + str(v_args[0])
