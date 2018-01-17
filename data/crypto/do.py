398a5c317323e6701044cc476c7ccac8d137a16e
David Cottrell <cottrell@users.noreply.github.com>
2018-01-17 23:03:38 +0000

    update

--- data/crypto/do.py
+++ data/crypto/do.py
@@ -15,38 +15,43 @@ def get_pandas_read_csv_defaults():
 
 _mydir = os.path.dirname(os.path.realpath('__file__'))
 
-kwargs = get_pandas_read_csv_defaults()
-kwargs['thousands'] = ',' # always do this
-# kwargs['parse_dates'] = ['Date']
-kwargs['na_values'] = ['-']
-kwargs['dtype'] = 'str'
-dtype = {
- 'Close': 'float',
- 'High': 'float',
- 'Low': 'float',
- 'Market Cap': 'float',
- 'Open': 'float',
- 'Volume': 'float'
- }
+def load_raw():
+    # note manually removed some bad row
+    kwargs = get_pandas_read_csv_defaults()
+    kwargs['thousands'] = ',' # always do this
+    kwargs['parse_dates'] = ['Date']
+    kwargs['na_values'] = ['-']
+    kwargs['dtype'] = 'str'
+    dtype = {
+     'Close': 'float',
+     'High': 'float',
+     'Low': 'float',
+     'Market Cap': 'float',
+     'Open': 'float',
+     'Volume': 'float'
+     }
 
-meta = pd.read_csv(os.path.join(_mydir, 'Top100Cryptos/data/100 List.csv'))
-names = meta.Name.tolist()
-files = [os.path.join(_mydir, 'Top100Cryptos/data/{}.csv'.format(x)) for x in names]
-# files = glob.glob(os.path.join(_mydir, 'Top100Cryptos/data/*.csv'))
-dfs = list()
-datadir = os.path.join(_mydir, 'parsed')
-if not os.path.exists(datadir):
-    os.makedirs(datadir)
-for i, (name, f) in enumerate(zip(names, files)):
-    mtime = os.path.getmtime(f)
-    dirname = os.path.join(datadir, 'name={}/mtime={}'.format(name, mtime))
-    filename = os.path.join(dirname, 'data.parquet')
-    if not os.path.exists(filename):
-        df = pd.read_csv(f, **kwargs)
-        df = pa.Table.from_pandas(df)
-        if not os.path.exists(dirname):
-            os.makedirs(dirname)
-        print('writing {}'.format(filename))
-        pq.write_table(df, filename)
-    pq.read_table('./parsed')
+    meta = pd.read_csv(os.path.join(_mydir, 'Top100Cryptos/data/100 List.csv'))
+    names = meta.Name.tolist()
+    files = [os.path.join(_mydir, 'Top100Cryptos/data/{}.csv'.format(x)) for x in names]
+    # files = glob.glob(os.path.join(_mydir, 'Top100Cryptos/data/*.csv'))
+    dfs = list()
+    datadir = os.path.join(_mydir, 'parsed')
+    if not os.path.exists(datadir):
+        os.makedirs(datadir)
+    for i, (name, f) in enumerate(zip(names, files)):
+        mtime = os.path.getmtime(f)
+        dirname = os.path.join(datadir, 'name={}/mtime={}'.format(name, mtime))
+        filename = os.path.join(dirname, 'data.parquet')
+        if not os.path.exists(filename):
+            df = pd.read_csv(f, **kwargs)
+            df = pa.Table.from_pandas(df)
+            if not os.path.exists(dirname):
+                os.makedirs(dirname)
+            print('writing {}'.format(filename))
+            pq.write_table(df, filename)
+            pq.read_table('./parsed') # test
+        else:
+            print('{} exists'.format(filename))
+    return pq.read_table('./parsed') # test
 
