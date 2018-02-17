34edf8b0e88d11820b35ddc9fbf2c46ab3ec4329
David Cottrell <cottrell@users.noreply.github.com>
2018-02-17 22:39:51 +0000

    update

--- data/routerlogs/whois_dossing.sh
+++ data/routerlogs/whois_dossing.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 [[ -d $DIR/whois ]] || mkdir -p $DIR/whois
 
-for x in $(grep DoS $DIR/20*.log | cut -d: -f6- | cut -d' ' -f2 | cut -d: -f1 | sort -u); do
+for x in $(grep DoS $DIR/20*.log | sort -u | cut -d: -f6- | cut -d' ' -f2 | cut -d: -f1 | sort -u); do
     if [[ ! -e $DIR/whois/$x ]]; then
         echo whois $x
         whois $x > $DIR/whois/$x
