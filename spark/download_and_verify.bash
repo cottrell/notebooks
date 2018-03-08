241a35b0ad8d792480169ad1ef4f04cf8b953e02
David Cottrell <cottrell@users.noreply.github.com>
2018-03-08 19:57:41 +0000

    spark

--- spark/download_and_verify.bash
+++ spark/download_and_verify.bash
@@ -8,13 +8,13 @@ function maybe_get {
     [[ -e "$base" ]] || wget $1
 }
 
-version=2.2.0
+version=2.3.0
 
-maybe_get http://d3kbcqa49mib13.cloudfront.net/spark-"$version"-bin-hadoop2.7.tgz
-maybe_get https://www.apache.org/dist/spark/KEYS
-maybe_get http://www.apache.org/dist/spark/spark-"$version"/spark-"$version"-bin-hadoop2.7.tgz.asc
-maybe_get http://www.apache.org/dist/spark/spark-"$version"/spark-"$version"-bin-hadoop2.7.tgz.md5
-maybe_get http://www.apache.org/dist/spark/spark-"$version"/spark-"$version"-bin-hadoop2.7.tgz.sha
+maybe_get  http://www.apache.org/dist/spark/spark-$version/spark-"$version"-bin-hadoop2.7.tgz
+maybe_get https://www.apache.org/dist/spark/spark-$version/KEYS
+maybe_get  http://www.apache.org/dist/spark/spark-$version/spark-"$version"/spark-"$version"-bin-hadoop2.7.tgz.asc
+maybe_get  http://www.apache.org/dist/spark/spark-$version/spark-"$version"/spark-"$version"-bin-hadoop2.7.tgz.md5
+maybe_get  http://www.apache.org/dist/spark/spark-$version/spark-"$version"/spark-"$version"-bin-hadoop2.7.tgz.sha
 
 md5_file=$(md5 spark-"$version"-bin-hadoop2.7.tgz | cut -d= -f2 | sed -e 's/ //' | tr '[:upper:]' '[:lower:]')
 md5_orig=$(cat spark-"$version"-bin-hadoop2.7.tgz.md5 | cut -d: -f2 | sed -e 's/ //g' | tr '[:upper:]' '[:lower:]')
@@ -33,6 +33,6 @@ else
     echo untarring to ~/dev
     tar -xzf ./spark-$version-bin-hadoop2.7.tgz -C ~/dev
 fi
-echo clean up: rm -rf KEYS spark-2.2.0-bin-hadoop2.7.tgz spark-2.2.0-bin-hadoop2.7.tgz.asc spark-2.2.0-bin-hadoop2.7.tgz.md5 spark-2.2.0-bin-hadoop2.7.tgz.sha
+echo clean up: rm -rf KEYS spark-$version-bin-hadoop2.7.tgz spark-$version-bin-hadoop2.7.tgz.asc spark-$version-bin-hadoop2.7.tgz.md5 spark-$version-bin-hadoop2.7.tgz.sha
 echo PATH='$PATH':$HOME/dev/spark-"$version"-bin-hadoop2.7/bin
 echo alias "pyspark_ipython='PYSPARK_DRIVER_PYTHON=ipython pyspark'"
