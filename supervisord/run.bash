3266b2f7940a0982949649172793750957805dc1
David Cottrell <cottrell@users.noreply.github.com>
2018-04-07 20:27:00 +0100

    update

--- supervisord/run.bash
+++ supervisord/run.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 trap "kill -- -$$" EXIT
 export DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
-echo running supervisord -c $DIR/supervisord.conf
+export ARXIVSANITYPATH=~/dev/arxiv-sanity-preserver
 supervisord -c $DIR/supervisord.conf
+tail -f /tmp/supervisord.log
 echo 'then run supervisorctl'
-# start thegroup:*
