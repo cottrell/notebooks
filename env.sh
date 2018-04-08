84d4e200bcca3b9823f373d48dabbde50f507ce7
David Cottrell <cottrell@users.noreply.github.com>
2018-04-08 17:31:26 +0100

    update

---
+++ env.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+ENVDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PYTHONPATH=$ENVDIR:$PYTHONPATH
--- notebook.sh
+++ notebook.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. $DIR/env.sh
 cd
 jupyter lab --NotebookApp.iopub_data_rate_limit=10000000
