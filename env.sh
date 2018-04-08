d3d308367901cb8f693ace7f45be265e9a4a92dc
David Cottrell <cottrell@users.noreply.github.com>
2018-04-08 18:42:30 +0100

    update

--- env.sh
+++ env.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
+# SOURCE THIS OR RUN IT!
 ENVDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SPARK_HOME=~/dev/spark-2.3.0-bin-hadoop2.7
+PATH=$PATH:$SPARK_HOME/bin
+PYTHONPATH=$PYTHONPATH:$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.10.6-src.zip
 PYTHONPATH=$ENVDIR:$PYTHONPATH
+
+case $1 in
+    pyspark_ipython)
+        export PYSPARK_DRIVER_PYTHON=ipython
+        pyspark \
+            --conf spark.sql.shuffle.partitions=8 \
+            --conf spark.sql.execution.arrow.enabled=true \
+            --conf spark.driver.memory='16g'
+        ;;
+    *)
+        ;;
+esac
--- pyspark_ipython.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export PYSPARK_DRIVER_PYTHON=ipython
-pyspark \
-    --conf spark.sql.shuffle.partitions=8 \
-    --conf spark.sql.execution.arrow.enabled=true \
-    --conf spark.driver.memory='16g'
