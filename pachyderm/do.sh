96a1fcef6c04aa575ec3da1bb6b48628ced8da72
David Cottrell <cottrell@users.noreply.github.com>
2018-05-07 10:57:01 +0100

    update

---
+++ pachyderm/do.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# had to force the link to update it
+case $1 in
+    install)
+        brew install kubectl
+        brew link --overwrite kubernetes-cli
+        if [ "$(uname)" = "Darwin" ]; then
+            brew tap pachyderm/tap && brew install pachyderm/tap/pachctl@1.7
+        else
+            curl -o /tmp/pachctl.deb -L https://github.com/pachyderm/pachyderm/releases/download/v1.7.1/pachctl_1.7.1_amd64.deb && sudo dpkg -i /tmp/pachctl.deb
+        fi
+        ;;
+    minikube_start)
+        minikube stop
+        minikube delete
+        minikube start
+        ;;
+    pachctl_start)
+        pachctl deploy local
+        kubectl get pods
+        pachctl port-forward &
+        ;;
+    *)
+        echo "do.sh install|pachctl_start|minikube_start"
+        ;;
+esac
+
+# get errors unable to decode "STDIN": no kind "ClusterRole" is registered for version "rbac.authorization.k8s.io/v1"
