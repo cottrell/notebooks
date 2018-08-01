34f201cc90ef82117ef19c56c7a048100519eb15
David Cottrell <cottrell@users.noreply.github.com>
2018-08-01 13:47:58 +0100

    update

--- postgres/run.sh
+++ postgres/run.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 export DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+dbdir=/var/tmp/postgres_test_db
+logfile=$DIR/pg.log
+
+mkdir -p $dbdir
+
 case $1 in
+    init)
+        pg_ctl -D /var/tmp/postgres_test_db -l $logfile initdb
+        ;;
     start)
-        pg_ctl -D /var/tmp/postgres_test_db -l /var/log/postgres_test_db.log start
+        pg_ctl -D /var/tmp/postgres_test_db -l $logfile start
         ;;
     stop)
         pg_ctl -D /var/tmp/postgres_test_db stop
