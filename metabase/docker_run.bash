1c3ae7c5ff2f494b82b6e9036cacbc72168cd7ea
David Cottrell <cottrell@users.noreply.github.com>
2018-08-01 10:01:05 +0100

    update

---
+++ metabase/docker_run.bash
@@ -0,0 +1,4 @@
+#!/bin/sh
+# need to map port on osx (I didn't do anything and it seems to have worked, maybe already have this setup)
+# https://stackoverflow.com/questions/36286305/how-do-i-forward-a-docker-machine-port-to-my-host-port-on-osx
+docker run -d -p 3000:3000 --name metabase metabase/metabase
