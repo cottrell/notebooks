b7e916f67a18cef6cb3351b1c913c404c29e9d36
David Cottrell <cottrell@users.noreply.github.com>
2018-08-23 10:11:06 +0100

    update

--- data/tax/example5.py
+++ data/tax/example5.py
@@ -1,17 +1,75 @@
+import os
+from pylab import *
+import seaborn as sns
+ion()
+from toolz import curry
 import pandas as pd
 import scipy.optimize as so
 import functools
 import numpy.random as nr
 import numpy as np
 from mylib import bok
+import mylib.plotting as plotting
 from do import F as F_, F_ni, G, _max_x
+import webbrowser
 F = lambda x: F_(x) + F_ni(x)
+# x and y
+_mydir = os.path.realpath(os.path.dirname(__file__))
 
-# complex one
+def multiplot(force=True):
+    t = 2
+    dirname = os.path.join(_mydir, 'plots')
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+    for A in range(10000, 310000, 1000):
+        filename = os.path.join(dirname, 'wireframe_t={}_A={}.png'.format(t, A))
+        if force or not os.path.exists(filename):
+            doplot(t=t, A=A, filename=filename)
+    gif = os.path.join(dirname, 'wireframe_t={}.gif'.format(t))
+    pngs = os.path.join(dirname, 'wireframe_t={}_A=*.png'.format(t))
+    cmd = 'cd {} && convert -loop 0 -delay 10 {} {}'.format(dirname, pngs, gif)
+    print('running: {}'.format(cmd))
+    # os.system(cmd)
+    # webbrowser.open('file://{}'.format(gif)) # opens in preview ugh
 
-def F_const(t, X, Y, alphas):
-    r = _F_const_full(t, X, Y, alphas)
-    return sum(r['tax'])
+def doplot(t=2, A=200000, filename=None, num=1):
+    assert t == 2, 'will not work otherwise'
+    X = A * t
+    Y = 0
+    f = f_opt(t, X, Y)
+    d = list()
+    n = 20
+    for alpha_x in linspace(0, 1, n + 1):
+        for alpha_y in linspace(0, 1, n + 1):
+            v = f([alpha_x, alpha_y])
+            d.append([alpha_x, alpha_y, v])
+    d = pd.DataFrame(d, columns='alpha_x,alpha_y,tax'.split(','))
+    df = d.set_index(['alpha_x', 'alpha_y'])['tax'].unstack('alpha_y')
+    # figure(num)
+    # clf()
+    # sns.heatmap(df)
+
+    from mpl_toolkits import mplot3d
+    import matplotlib.pyplot as plt
+    fig = plt.figure(num)
+    ax = plt.axes(projection='3d')
+    xx, yy, zz = meshgrid_from_df(df)
+    ax.plot_wireframe(xx, yy, zz, color='black')
+    ax.set_xlabel(df.index.names[0])
+    ax.set_ylabel(df.columns.names[0])
+    ax.set_title('t = {}, A = {}'.format(t, A))
+    if filename is not None:
+        print('saving {}'.format(filename))
+        savefig(filename)
+    return d, ax
+
+@curry
+def f_opt(t, X, Y, alphas_ravelled):
+    alphas = unravel(np.array(alphas_ravelled))
+    return sum(_F_const_full(t, X, Y, alphas)['tax'])
+
+def unravel(actions):
+    return actions.reshape((int(actions.shape[0] / 2), 2))
 
 def _F_const_full(t, X, Y, alphas):
     alphas = np.atleast_2d(np.array(alphas))
@@ -42,11 +100,6 @@ def _F_const_full(t, X, Y, alphas):
         assert Y >= 0
     return dict(tax=tax, xs=xs, ys=ys, Ys=Ys, Xs=Xs, alpha_xs=alpha_xs, alpha_ys=alpha_ys)
 
-# test
-t = 3
-X = 200000 * t
-Y = 0
-# r = _F_const_full(t, X, Y, np.array([[.3, .3], [1, 1]]))
 def get_var(t, X, n=1000):
     X = X * t
     d = list()
@@ -57,9 +110,6 @@ def get_var(t, X, n=1000):
     s = d.describe()
     return (s['max'] - s['min']) / t
 
-# from pylab import *
-# ion()
-# 
 # def doplot(X):
 #     figure(1)
 #     clf()
@@ -73,9 +123,3 @@ def get_var(t, X, n=1000):
 #         df.append(d)
 #     df = pd.DataFrame(df)
 #     return df
-# 
-# figure(1)
-# clf()
-# df = doplot(110000)
-# plot(df.alpha_y, df.tax)
-# show()
