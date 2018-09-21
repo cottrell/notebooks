1632c100c2ca4ff7b4935d6a6778698612edc8cd
David Cottrell <cottrell@users.noreply.github.com>
2018-09-21 11:16:47 +0100

    update

---
+++ hugo/README.md
@@ -0,0 +1,3 @@
+# setup
+
+    cd hugoblog && ../do.sh serve
--- hugo/do.sh
+++ hugo/do.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 case $1 in
     init)
         hugo new site .
-        # git init # assume you are in git repo already
+        git init
         # git submodule add https://github.com/budparr/gohugo-theme-ananke.git themes/ananke
         # echo 'theme = "ananke"' >> config.toml
         git submodule add https://github.com/alanorth/hugo-theme-bootstrap4-blog.git themes/hugo-theme-bootstrap4-blog
