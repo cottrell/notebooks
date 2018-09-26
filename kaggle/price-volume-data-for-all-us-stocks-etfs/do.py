819a56777d64632b288bb2bb12e5698022867b03
David Cottrell <cottrell@users.noreply.github.com>
2018-09-26 17:04:40 +0100

    Ongoing etf

--- kaggle/price-volume-data-for-all-us-stocks-etfs/do.py
+++ kaggle/price-volume-data-for-all-us-stocks-etfs/do.py
@@ -25,40 +25,42 @@ def run_raw(nrows=None, force=False):
     if force:
         if os.path.exists(outfile):
             shutil.rmtree(outfile)
-    if not os.path.exists(outfile):
-        for k in ['ETFs', 'Stocks']:
-            data = list()
-            product = k.lower()
-            filenames = raw_source()
-            t = time.time()
-            i = 0
-            for filename in filenames:
-                name, market = os.path.basename(filename).split('.')[:2]
-                product, market, name, filename, dict(nrows=nrows)
-                lc = mylib.io.get_capped_line_count(filename)
-                if lc >= 2:
-                    df = pd.read_csv(filename, nrows=nrows, encoding='utf-8')
-                    df['product'] = product
-                    df['market'] = market
-                    df['name'] = name
-                    df['Date'] = pd.to_datetime(df.Date)
-                    data.append(df)
-                else:
-                    print('skipping {}'.format(filename))
-                i += 1
-                if i % 10 == 0:
-                    print('{} of {} eta {} seconds for {}'.format(i, len(filenames), (time.time() - t) * (len(filenames) - i) / i, product))
-            df = pd.concat(data, axis=0)
-            df = df.sort_values(['name', 'Date'])
-            df = df.drop(['market', 'OpenInt'], axis=1)
-            df.columns = [x.lower() for x in df.columns]
-            print('writing {}'.format(outfile))
-            # plain to_parquet seems to be always writing the index, also partition_cols not in pandas yet
-            table = pa.Table.from_pandas(df, preserve_index=False)
-            # partitioning by name is less efficient storage wise but makes for better joins in the next step
-            pq.write_to_dataset(table, root_path=outfile, partition_cols=['product', 'name'], preserve_index=False)
-            # pq.write_table(table, outfile)
-            # df.to_parquet(outfile, index=False)
+    if os.path.exists(outfile):
+        print('{} exists. force=True to rerun'.format(outfile))
+        return
+    for k in ['ETFs', 'Stocks']:
+        data = list()
+        product = k.lower()
+        filenames = raw_source()
+        t = time.time()
+        i = 0
+        for filename in filenames:
+            name, market = os.path.basename(filename).split('.')[:2]
+            product, market, name, filename, dict(nrows=nrows)
+            lc = mylib.io.get_capped_line_count(filename)
+            if lc >= 2:
+                df = pd.read_csv(filename, nrows=nrows, encoding='utf-8')
+                df['product'] = product
+                df['market'] = market
+                df['name'] = name
+                df['Date'] = pd.to_datetime(df.Date)
+                data.append(df)
+            else:
+                print('skipping {}'.format(filename))
+            i += 1
+            if i % 10 == 0:
+                print('{} of {} eta {} seconds for {}'.format(i, len(filenames), (time.time() - t) * (len(filenames) - i) / i, product))
+        df = pd.concat(data, axis=0)
+        df = df.sort_values(['name', 'Date'])
+        df = df.drop(['market', 'OpenInt'], axis=1)
+        df.columns = [x.lower() for x in df.columns]
+        print('writing {}'.format(outfile))
+        # plain to_parquet seems to be always writing the index, also partition_cols not in pandas yet
+        table = pa.Table.from_pandas(df, preserve_index=False)
+        # partitioning by name is less efficient storage wise but makes for better joins in the next step
+        pq.write_to_dataset(table, root_path=outfile, partition_cols=['product', 'name'], preserve_index=False)
+        # pq.write_table(table, outfile)
+        # df.to_parquet(outfile, index=False)
     # print('reading {}'.format(outfile))
     # return pd.read_parquet(outfile)
 
@@ -84,17 +86,21 @@ def enrich_pandas_single(df, inplace=True):
         for k in ['close']:
             df[k + '_rolling_min_{}d'.format(i)] = r[k].min()
             df[k + '_rolling_max_{}d'.format(i)] = r[k].max()
-            df['future_' + k + '_rolling_min_{}d'.format(i)] = df[k + '_rolling_min_{}d'.format(i)].shift(-i)
-            df['future_' + k + '_rolling_max_{}d'.format(i)] = df[k + '_rolling_min_{}d'.format(i)].shift(-i)
+            # the min/max returns on a i-day period
+            df['future_' + k + '_max_loss_{}d'.format(i)] = df[k + '_rolling_min_{}d'.format(i)].shift(-i) / df.close - 1
+            df['future_' + k + '_max_gain_{}d'.format(i)] = df[k + '_rolling_max_{}d'.format(i)].shift(-i) / df.close - 1
     return df
 
 import dask.bag as db
-def run_enriched(nrows=None):
+def run_enriched(nrows=None, force=False):
     input_dirname = enriched_source(nrows=nrows)
     filenames = glob.glob(os.path.join(input_dirname, 'product=etfs/name=*/')) # limit to etfs for now
     output_dirname = enriched_target(nrows=nrows)
+    if force and os.path.exists(output_dirname):
+        shutil.rmtree(output_dirname)
     if os.path.exists(output_dirname):
-        os.system('dl {}'.format(output_dirname))
+        print('{} exists. force=True to rerun'.format(output_dirname))
+        return
     bag = db.from_sequence(filenames)
     output_filenames = bag.map(lambda x: os.path.join(output_dirname, os.path.relpath(x, input_dirname))).compute() # no need for delayed on glob really
     def read_transform_write(infile, outfile):
@@ -104,7 +110,7 @@ def run_enriched(nrows=None):
         table = pa.Table.from_pandas(df, preserve_index=False)
         pq.write_to_dataset(table, root_path=outfile, preserve_index=False)
     mapped = db.from_sequence(zip(filenames, output_filenames)).starmap(read_transform_write)
-    mappend.compute()
+    mapped.compute()
 
 # def enrich(df):
 #     import pyspark.sql.functions as F
