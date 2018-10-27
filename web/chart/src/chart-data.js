e1326c63fae3582d121bf186805137da93457642
David Cottrell <cottrell@users.noreply.github.com>
2018-10-27 15:19:31 +0100

    update

--- web/chart/src/chart-data.js
+++ web/chart/src/chart-data.js
@@ -1,10 +1,11 @@
 export const planetChartData = {
-  type: 'line',
+  type: 'bar',
   data: {
     labels: ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'],
     datasets: [
       { // one line graph
         label: 'Number of Moons',
+        type: 'bar',
         data: [0, 0, 1, 2, 67, 62, 27, 14],
         backgroundColor: [
           'rgba(54,73,93,.5)', // Blue
@@ -30,6 +31,7 @@ export const planetChartData = {
       },
       { // another line graph
         label: 'Planet Mass (x1,000 km)',
+        type: 'line',
         data: [4.8, 12.1, 12.7, 6.7, 139.8, 116.4, 50.7, 49.2],
         backgroundColor: [
           'rgba(71, 183,132,.5)', // Green
