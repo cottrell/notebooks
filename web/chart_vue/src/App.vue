597a086965de1f02a021ecf015dadf846186c5e3
David Cottrell <cottrell@users.noreply.github.com>
2018-10-29 08:55:00 +0000

    Revert "update"
    This reverts commit 5b1b2f22ef2716234169c1f600c27aabb1c59fa5.

--- web/chart_vue/src/App.vue
+++ web/chart_vue/src/App.vue
@@ -1,15 +1,35 @@
 <template>
   <div id="app">
+    <canvas id="planet-chart"></canvas>
   </div>
 </template>
 
 <script>
+import Chart from 'chart.js';
+import planetChartData from './chart-data.js';
+
+
 export default {
   name: 'app',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
+      planetChartData: planetChartData,
+
     }
+  },
+  methods: {
+    createChart(chartId, chartData) {
+      const ctx = document.getElementById(chartId);
+      const myChart = new Chart(ctx, {
+        type: chartData.type,
+        data: chartData.data,
+        options: chartData.options,
+      });
+    }
+  },
+  mounted() {
+    this.createChart('planet-chart', this.planetChartData);
   }
 }
 </script>
