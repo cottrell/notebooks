5b1b2f22ef2716234169c1f600c27aabb1c59fa5
David Cottrell <cottrell@users.noreply.github.com>
2018-10-29 08:54:15 +0000

    update

--- web/hello/src/App.vue
+++ web/hello/src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
   name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-    }
+  components: {
+    HelloWorld
   }
 }
 </script>
