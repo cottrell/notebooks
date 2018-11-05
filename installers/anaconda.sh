10074ff09b75d46afded85b3c1855218525aa895
David Cottrell <cottrell@users.noreply.github.com>
2018-11-05 18:26:31 +0000

    update

--- installers/anaconda.sh
+++ installers/anaconda.sh
@@ -1,4 +1,19 @@
-#!/bin/sh
+#!/bin/sh -e
+
+if [[ $(uname) -eq Linux ]]; then
+    filename=Anaconda3-5.3.0-MacOSX-x86_64.sh
+else
+    filename=Anaconda3-5.3.0-Linux-x86_64.sh
+fi
+echo $filename
 
 cd /tmp
-wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86.sh
+if [[ -e $filename ]]; then
+    echo $filename exists
+else
+    wget https://repo.anaconda.com/archive/$filename
+fi
+
+bash ./$filename -u
+
+~/anaconda3/bin/conda create -n 37 pandas
