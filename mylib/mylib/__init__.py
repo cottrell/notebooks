3b3e1c24fbf6444566c094346936798947a6adfa
David Cottrell <cottrell@users.noreply.github.com>
2018-11-12 18:34:59 +0000

    mylib

--- mylib/mylib/__init__.py
+++ mylib/mylib/__init__.py
@@ -20,3 +20,20 @@ def wok():
     print("wak vars from {}".format(name))
     sys._getframe(-1).f_back.f_locals.update(_locals)
 
+
+def dict_from_locals(text_or_list_of_text):
+    d = dict()
+    frame = sys._getframe(-1)
+    _locals = frame.f_back.f_locals
+    if isinstance(text_or_list_of_text, str):
+        text_or_list_of_text = text_or_list_of_text.split(',')
+    for k in text_or_list_of_text:
+        k = k.strip()
+        d[k] = _locals[k]
+    return d
+
+def test_dict_from_locals():
+    a = 1
+    b = 2
+    r = dict_from_locals('a,b')
+    assert r == {'a': 1, 'b': 2}
