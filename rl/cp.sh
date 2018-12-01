9c2edca2214d52fd6c06e159a96f0cc58855d38e
David Cottrell <cottrell@users.noreply.github.com>
2018-12-01 18:11:28 +0000

    update

--- rl/cp.sh
+++ rl/cp.sh
@@ -3,15 +3,12 @@
 # because things are not importable as they are
 # example: cp.sh code/Chapter02/*py
 
-for x in $*; do
-    a=$(basename $x | cut -d'_' -f1)
-    b=$(basename $x | cut -d'_' -f2- | cut -d'.' -f1)
-    c=$(basename $(dirname $x) | sed -e 's/Chapter//')
-    filename="ch$c"_"$a"_$b.py
-    if [ -e $filename ]; then
-        echo $c
-        echo "$filename exists!"
+for x in */Chapter*; do
+    c=$(basename $x | sed -e 's/Chapter//')
+    if [ -e ch$c ]; then
+        echo ch$c exists!
     else
-        cp -v $x $filename
+        cp -v -R $x ch$c
+        touch ch$c/__init__.py
     fi
 done
