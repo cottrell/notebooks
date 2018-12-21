2f4a1098e4d21a2c641c35ee6c6abb3802b4a8c9
David Cottrell <cottrell@users.noreply.github.com>
2018-12-21 12:02:30 +0000

    Add fx

--- extractors/extractors/pdr.py
+++ extractors/extractors/pdr.py
@@ -28,19 +28,37 @@ _yahoo_product_map = _get_yahoo_product_map()
 # 2000 per hour max, my version of pandas DataReader is throttled but count is not persisted
 
 _maybe_inactive = dict()
-def get_all_yahoo(period='oneweek'):
+def get_all_yahoo_equities_etfs(period='oneweek'):
+    """ use period None for full history """
+    _get_all_yahoo('equities/etfs', period=period)
+def get_all_yahoo_fx(period='oneweek'):
+    """ use period None for full history """
+    _get_all_yahoo('fx', period=period)
+
+def _get_all_yahoo(product, period='oneweek'):
     """ use period None for full history """
     global _maybe_inactive
     kwargs = {}
     start, end = lib.render_date_arg(period)
-    for symbol in _yahoo_product_map:
-        try:
-            get_yahoo_price_volume(symbol, start=start, end=end)
-        except Exception as e:
-            _maybe_inactive[symbol] = str(e)
-    json.dump(_maybe_inactive, open('yahoo_errors.json', 'w'))
+    if product == 'equities/etfs':
+        for symbol in _yahoo_product_map:
+            try:
+                get_yahoo_price_volume(symbol, start=start, end=end)
+            except Exception as e:
+                _maybe_inactive[symbol] = str(e)
+        json.dump(_maybe_inactive, open('yahoo_errors_equities_etfs.json', 'w'))
+    elif product == 'fx':
+        bad_fx = dict()
+        for x in _usd_pairs:
+            try:
+                get_yahoo_fx(x, start=start, end=end)
+            except Exception as e:
+                print('skipping {}: {}'.format(x, e))
+                bad_fx[x] = str(e)
+        json.dump(bad_fx, open('yahoo_errors_fx.json', 'w'))
 
 
+_usd_pairs = ['EURUSD', 'JPYUSD', 'GBPUSD', 'CHFUSD', 'AUDUSD', 'NZDUSD', 'CADUSD', 'SEKUSD', 'NOKUSD', 'CZKUSD', 'EGPUSD', 'HUFUSD', 'ISKUSD', 'ILSUSD', 'PLNUSD', 'RONUSD', 'RUBUSD', 'ZARUSD', 'TRYUSD', 'UAHUSD', 'KWDUSD', 'SARUSD', 'AEDUSD', 'BHDUSD', 'OMRUSD', 'QARUSD', 'CNYUSD', 'HKDUSD', 'INRUSD', 'IDRUSD', 'KZTUSD', 'KRWUSD', 'MYRUSD', 'PHPUSD', 'SGDUSD', 'TWDUSD', 'THBUSD', 'VNDUSD', 'ARSUSD', 'BRLUSD', 'CLPUSD', 'COPUSD', 'MXNUSD', 'PENUSD']
 
 @lib.extractor(partition_cols=['product', 'symbol'])
 def get_yahoo_price_volume(symbol, start=None, end=None):
@@ -61,6 +79,22 @@ def get_yahoo_price_volume(symbol, start=None, end=None):
     df = pdr.DataReader(symbol, data_source='yahoo', start=start, end=end, get_actions=False).reset_index()
     lib.apply_schema_to_df_inplace(df, schema)
     yield {'product': product, 'symbol': symbol}, df
+
+@lib.extractor(partition_cols=['symbol'])
+def get_yahoo_fx(symbol, start=None, end=None):
+    start, end = lib.render_date_arg(start, end)
+    # pdr should have a rate limit in my hacked version
+    # need to fix scheme/enforce etfs/stocks different
+    schema = {'Date': 'datetime64[ns]',
+            'High': 'float64',
+            'Low': 'float64',
+            'Open': 'float64',
+            'Close': 'float64',
+            'ingress_time': 'datetime64[ns]'
+            }
+    df = pdr.yahoo.fx.YahooFXReader(symbol, start=start, end=end).read().reset_index()
+    lib.apply_schema_to_df_inplace(df, schema)
+    yield {'symbol': symbol}, df
 
 # def fix():
 #     schema = {'Date': 'datetime64[ns]',
