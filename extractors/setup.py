2f5c79c06ad2e4f45eeffc14b76427a37793a13b
David Cottrell <cottrell@users.noreply.github.com>
2018-12-21 10:02:10 +0000

    update

--- extractors/setup.py
+++ extractors/setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(name='extractors',
       version='0.1',
       description='extracting data',
+      install_requires=['ratelimit'],
       packages=['extractors'],
       zip_safe=False)
