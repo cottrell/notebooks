0a5231e1066e79ad7f2bd3d984f4da14e8fbcb12
David Cottrell <cottrell@users.noreply.github.com>
2019-01-16 18:21:08 +0000

    Move extractors to own repo.

--- extractors/extractors/imf.py
+++
@@ -1,7 +0,0 @@
-"""
-CDIS: http://data.imf.org/?sk=40313609-F037-48C1-84B1-E1F1CE54D6D5
-CIPS: http://data.imf.org/?sk=B981B4E3-4E58-467E-9B90-9DE0C3367363
-IFS: http://data.imf.org/?sk=4C514D48-B6BA-49ED-8AB9-52B0C1A0179B
-GFS: http://data.imf.org/?sk=a0867067-d23c-4ebc-ad23-d3b015045405
-"""
-# TODO
--- extractors/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='extractors',
-      version='0.1',
-      description='extracting data',
-      install_requires=['ratelimit'],
-      packages=['extractors', 'quandl'],
-      zip_safe=False)
