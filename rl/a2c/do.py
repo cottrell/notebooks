994f9ec928325d7f75dfd755bd06845058460c5d
David Cottrell <cottrell@users.noreply.github.com>
2019-01-20 21:52:06 +0000

    update

--- rl/a2c/do.py
+++ rl/a2c/do.py
@@ -43,3 +43,113 @@ obs = env.reset()
 # no feed_dict or tf.Session() needed at all
 action, value = model.action_value(obs[None, :])
 print(action, value) # [1] [-0.00145713]
+
+import tensorflow.keras.losses as kls
+import tensorflow.keras.optimizers as ko
+
+class A2CAgent:
+    def __init__(self, model):
+        # hyperparameters for loss terms
+        self.params = {'value': 0.5, 'entropy': 0.0001, 'gamma': 0.99}
+        self.model = model
+        self.model.compile(
+            optimizer=ko.RMSprop(lr=0.0007),
+            # define separate losses for policy logits and value estimate
+            loss=[self._logits_loss, self._value_loss]
+        )
+
+    def test(self, env, render=True):
+        obs, done, ep_reward = env.reset(), False, 0
+        while not done:
+            action, _ = self.model.action_value(obs[None, :])
+            obs, reward, done, _ = env.step(action)
+            ep_reward += reward
+            if render:
+                env.render()
+        return ep_reward
+
+    # LOSS / OBJECTIVE
+    def _value_loss(self, returns, value):
+        # value loss is typically MSE between value estimates and returns
+        return self.params['value']*kls.mean_squared_error(returns, value)
+
+    def _logits_loss(self, acts_and_advs, logits):
+        # a trick to input actions and advantages through same API
+        actions, advantages = tf.split(acts_and_advs, 2, axis=-1)
+        # polymorphic CE loss function that supports sparse and weighted options
+        # from_logits argument ensures transformation into normalized probabilities
+        cross_entropy = kls.CategoricalCrossentropy(from_logits=True)
+        # policy loss is defined by policy gradients, weighted by advantages
+        # note: we only calculate the loss on the actions we've actually taken
+        # thus under the hood a sparse version of CE loss will be executed
+        actions = tf.cast(actions, tf.int32)
+        policy_loss = cross_entropy(actions, logits, sample_weight=advantages)
+        # entropy loss can be calculated via CE over itself
+        entropy_loss = cross_entropy(logits, logits)
+        # here signs are flipped because optimizer minimizes
+        return policy_loss - self.params['entropy']*entropy_loss
+
+    # AGENT TRAINING LOOP
+    def train(self, env, batch_sz=32, updates=1000):
+        # storage helpers for a single batch of data
+        actions = np.empty((batch_sz,), dtype=np.int32)
+        rewards, dones, values = np.empty((3, batch_sz))
+        observations = np.empty((batch_sz,) + env.observation_space.shape)
+        # training loop: collect samples, send to optimizer, repeat updates times
+        ep_rews = [0.0]
+        next_obs = env.reset()
+        for update in range(updates):
+            for step in range(batch_sz):
+                observations[step] = next_obs.copy()
+                actions[step], values[step] = self.model.action_value(next_obs[None, :])
+                next_obs, rewards[step], dones[step], _ = env.step(actions[step])
+
+                ep_rews[-1] += rewards[step]
+                if dones[step]:
+                    ep_rews.append(0.0)
+                    next_obs = env.reset()
+
+            _, next_value = self.model.action_value(next_obs[None, :])
+            returns, advs = self._returns_advantages(rewards, dones, values, next_value)
+            # a trick to input actions and advantages through same API
+            acts_and_advs = np.concatenate([actions[:, None], advs[:, None]], axis=-1)
+            # performs a full training step on the collected batch
+            # note: no need to mess around with gradients, Keras API handles it
+            losses = self.model.train_on_batch(observations, [acts_and_advs, returns])
+        return ep_rews
+
+    def _returns_advantages(self, rewards, dones, values, next_value):
+        # next_value is the bootstrap value estimate of a future state (the critic)
+        returns = np.append(np.zeros_like(rewards), next_value, axis=-1)
+        # returns are calculated as discounted sum of future rewards
+        for t in reversed(range(rewards.shape[0])):
+            returns[t] = rewards[t] + self.params['gamma'] * returns[t+1] * (1-dones[t])
+        returns = returns[:-1]
+        # advantages are returns - baseline, value estimates in our case
+        advantages = returns - values
+        return returns, advantages
+
+agent = A2CAgent(model)
+
+def run_test():
+    rewards_sum = agent.test(env)
+    print("%d out of 200" % rewards_sum) # 18 out of 200
+
+
+def run_train():
+    rewards_history = agent.train(env)
+    print("Finished training, testing...")
+    print("%d out of 200" % agent.test(env)) # 200 out of 200
+
+def run_with_static_graph():
+    with tf.Graph().as_default():
+        print(tf.executing_eagerly()) # False
+
+        model = Model(num_actions=env.action_space.n)
+        agent = A2CAgent(model)
+
+        rewards_history = agent.train(env)
+        print("Finished training, testing...")
+        print("%d out of 200" % agent.test(env)) # 200 out of 200
+
+run_with_static_graph()
