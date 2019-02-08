d3f50bdcd628beac3805cabce790fb9f0f34f1fd
David Cottrell <cottrell@users.noreply.github.com>
2019-02-08 22:07:16 +0000

    move to mylib submoduel

--- mylib/README.md
+++
@@ -1,3 +0,0 @@
-# Tools
-
-Probably some strict requirements like pandas. TODO
--- tensorflow/mnist2.py
+++ tensorflow/mnist2.py
@@ -1,18 +1,30 @@
 import tensorflow as tf
 mnist = tf.keras.datasets.mnist
 
+def variable_summaries(var):
+    """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar('stddev', stddev)
+        tf.summary.scalar('max', tf.reduce_max(var))
+        tf.summary.scalar('min', tf.reduce_min(var))
+        tf.summary.histogram('histogram', var)
+
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(),
-  tf.keras.layers.Dense(512, activation=tf.nn.relu),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
-])
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation=tf.nn.relu),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
 model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy'])
 
 model.fit(x_train, y_train, epochs=5)
 model.evaluate(x_test, y_test)
