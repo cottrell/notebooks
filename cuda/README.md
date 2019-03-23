1a77a1d5509f365843587ff77a07e6b311ce29d3
David Cottrell <cottrell@users.noreply.github.com>
2019-03-23 16:51:28 +0000

    update

--- cuda/README.md
+++ cuda/README.md
@@ -1,3 +1,13 @@
+# Try this?
+
+https://www.tensorflow.org/install/gpu
+
+purge all and install the correct driver ! 410 see ubuntu/nvidia_purge.sh
+
+nvidia-410 and 10-0
+
+not sure if nvidia-418 auto includes 10-1
+
 # As of 2019-03
 
 Later version of nvidia drivers seem to be ok across the board for tf-nightly-gpu and tf 2.0.
@@ -6,6 +16,7 @@ Do a clean install/purge of nvidia and cuda (see ../ubuntu notes one level up).
 
 Download latest nvidia cuda driver manually and follow the instructions there that usually look something like this:
 
+    # NOT THIS ONE PROBABLY!
     sudo dpkg -i ./cuda-repo-ubuntu1810-10-1-local-10.1.105-418.39_1.0-1_amd64.deb
     sudo apt-key add /var/cuda-repo-10-1-local-10.1.105-418.39/7fa2af80.pub
     sudo apt-get update
@@ -17,3 +28,9 @@ Then there is some issue with 10-1 needing cusparse from 10-0, seems we need to 
     sudo apt-key add /var/cuda-repo-10-0-local-10.0.130-410.48/7fa2af80.pub
     sudo apt-get update
     sudo apt-get install cuda-10-0
+
+Then cudnn another manual download that requires login to nvidia. Make sure you GET THE LATEST NOT THE ARCHIVE.
+
+Remember the cudnn problems do not become apparent until you try conv etc.
+
+
--- ubuntu/nvidia_purge.sh
+++ ubuntu/nvidia_purge.sh
@@ -1,14 +1,48 @@
 #!/bin/sh -e
 
+# (36) cottrell@bleepblop:~$ nvidia-smi
+# Sat Mar 23 16:43:33 2019       
+# +-----------------------------------------------------------------------------+
+# | NVIDIA-SMI 410.104      Driver Version: 410.104      CUDA Version: 10.0     |
+# |-------------------------------+----------------------+----------------------+
+# | GPU  Name        Persistence-M| Bus-Id        Disp.A | Volatile Uncorr. ECC |
+# | Fan  Temp  Perf  Pwr:Usage/Cap|         Memory-Usage | GPU-Util  Compute M. |
+# |===============================+======================+======================|
+# |   0  GeForce RTX 2070    Off  | 00000000:01:00.0  On |                  N/A |
+# | 30%   36C    P0    44W / 175W |    279MiB /  7949MiB |      1%      Default |
+# +-------------------------------+----------------------+----------------------+
+#                                                                                
+# +-----------------------------------------------------------------------------+
+# | Processes:                                                       GPU Memory |
+# |  GPU       PID   Type   Process name                             Usage      |
+# |=============================================================================|
+# |    0      1609      G   /usr/lib/xorg/Xorg                            18MiB |
+# |    0      1682      G   /usr/bin/gnome-shell                          50MiB |
+# |    0      2531      G   /usr/lib/xorg/Xorg                            90MiB |
+# |    0      2665      G   /usr/bin/gnome-shell                         117MiB |
+# +-----------------------------------------------------------------------------+
+
+
+# NOT SURE NOT SURE MAYBE ONLY cuda 10.0 supported and nvidia driver 410?
+
+sudo apt purge ^nvidia
+sudo apt purge '*cuda*'
+sudo apt autoremove
+sudo add-apt-repository --remove ppa:graphics-drivers/ppa
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt update
+sudo apt install nvidia-driver-410
+
 # shoot me
 
 # generally you don't want to just run this, it times out and you don't need to repurge all the time
 sudo apt purge ^nvidia
-# sudo apt autoremove
-# sudo add-apt-repository --remove ppa:graphics-drivers/ppa
+sudo apt purge '*cuda*'
+sudo apt autoremove
+sudo add-apt-repository --remove ppa:graphics-drivers/ppa
 sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt update
 # sudo apt install nvidia-driver-396
-sudo ubuntu-drivers autoinstall
+# sudo ubuntu-drivers autoinstall
 echo check nvidia-smi after reboot
 echo now reboot
