68dd6b29320db8b6273dd46be9ddf2722c1bccc2
David Cottrell <cottrell@users.noreply.github.com>
2020-03-25 22:04:18 +0000

    update

--- tensorflow/check_gpu.py
+++ tensorflow/check_gpu.py
@@ -1,4 +1,7 @@
+#!/usr/bin/env python
 import tensorflow as tf
-print("cuda_only=False", tf.test.is_gpu_available(cuda_only=False, min_cuda_compute_capability=None))
-print("")
-print("cuda_only=True", tf.test.is_gpu_available(cuda_only=True, min_cuda_compute_capability=None))
+# print("cuda_only=False", tf.test.is_gpu_available(cuda_only=False, min_cuda_compute_capability=None))
+# print("")
+# print("cuda_only=True", tf.test.is_gpu_available(cuda_only=True, min_cuda_compute_capability=None))
+
+print(tf.config.list_physical_devices('GPU'))
--- ubuntu/nvidia_purge.sh
+++ ubuntu/nvidia_purge.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 # (36) cottrell@bleepblop:~$ nvidia-smi
-# Sat Mar 23 16:43:33 2019       
+# Sat Mar 23 16:43:33 2019
 # +-----------------------------------------------------------------------------+
 # | NVIDIA-SMI 410.104      Driver Version: 410.104      CUDA Version: 10.0     |
 # |-------------------------------+----------------------+----------------------+
@@ -11,7 +11,7 @@
 # |   0  GeForce RTX 2070    Off  | 00000000:01:00.0  On |                  N/A |
 # | 30%   36C    P0    44W / 175W |    279MiB /  7949MiB |      1%      Default |
 # +-------------------------------+----------------------+----------------------+
-#                                                                                
+#
 # +-----------------------------------------------------------------------------+
 # | Processes:                                                       GPU Memory |
 # |  GPU       PID   Type   Process name                             Usage      |
@@ -31,10 +31,11 @@ sudo apt autoremove
 sudo add-apt-repository --remove ppa:graphics-drivers/ppa
 sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt update
-sudo apt install nvidia-driver-410
 
+echo then follow tensorflow site instruction ... see ./cude/from_tf_site_2
+
+# sudo apt install nvidia-driver-410
 # shoot me
-
 # # generally you don't want to just run this, it times out and you don't need to repurge all the time
 # sudo apt purge ^nvidia
 # sudo apt purge '*cuda*'
