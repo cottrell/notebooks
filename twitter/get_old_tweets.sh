505f4bb2867e81b199c7ab82b0de151b6fb3eb5a
David Cottrell <cottrell@users.noreply.github.com>
2020-05-28 09:26:32 +0100

    update

--- twitter/get_old_tweets.sh
+++ twitter/get_old_tweets.sh
@@ -2,8 +2,9 @@
 # https://github.com/Mottl/GetOldTweets3
 if [[ $# -lt 1 ]]; then
     echo usage: prog username [since]
+    exit 1
 fi
 since=$2
-since="${since:-2019-09-01}"
+since="${since:-2020-01-01}"
 GetOldTweets3 --username $1 --since $since --emoji unicode --output get_old_$1.csv
 
