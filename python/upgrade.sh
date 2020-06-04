9aa35b76cff5754195bc243d9c00766604bea596
David Cottrell <cottrell@users.noreply.github.com>
2020-06-04 11:17:01 +0100

    Cleaning up setup.

--- python/upgrade.sh
+++ python/upgrade.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 pip install -r ./environment.pip -U
+# for some reason this fails in upgrade mode as of 2020-06-04
+pip install cvxpy
 
 jupyter labextension install @jupyterlab/vega3-extension
 
