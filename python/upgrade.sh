0057e7ecafcb003a96b7114345c5333bcbcb947b
David Cottrell <cottrell@users.noreply.github.com>
2020-06-05 09:12:07 +0100

    Forgotten gpu flip tf install and added vaex.

--- python/upgrade.sh
+++ python/upgrade.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-pip install -r ./environment.pip -U
+if [[ $(uname) = Darwin ]]; then
+    cat ./environment.pip | sed -e 's/tensorflow-gpu/tensorflow/' > /tmp/environment.pip
+    pip install -r /tmp/environment.pip -U
+else
+    pip install -r ./environment.pip -U
+fi
 
 jupyter labextension install @jupyterlab/vega3-extension
 
