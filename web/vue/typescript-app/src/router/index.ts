8082ba02c0b960d866960bcca3599245f6afc464
David Cottrell <cottrell@users.noreply.github.com>
2020-06-06 17:27:10 +0100

    Manually make new view.

--- web/vue/typescript-app/src/router/index.ts
+++ web/vue/typescript-app/src/router/index.ts
@@ -18,6 +18,15 @@ const routes: Array<RouteConfig> = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/main",
+    name: "Main",
+    // route level code-splitting
+    // this generates a separate chunk (main.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "main" */ "../views/Main.vue")
   }
 ];
 
