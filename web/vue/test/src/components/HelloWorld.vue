d22408264e799797d05dac8194379bf416449470
David Cottrell <cottrell@users.noreply.github.com>
2020-06-15 18:08:03 +0100

    update

--- web/vue/test/src/components/HelloWorld.vue
+++ web/vue/test/src/components/HelloWorld.vue
@@ -1,76 +1,151 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <my-chart v-bind:spec="first_spec"></my-chart>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Vuetify
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          For help and collaboration with other Vuetify developers,
+          <br>please join our online
+          <a
+            href="https://community.vuetifyjs.com"
+            target="_blank"
+          >Discord Community</a>
+        </p>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          What's next?
+        </h2>
+
+        <v-row justify="center">
+          <a
+            v-for="(next, i) in whatsNext"
+            :key="i"
+            :href="next.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ next.text }}
+          </a>
+        </v-row>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          Important Links
+        </h2>
+
+        <v-row justify="center">
+          <a
+            v-for="(link, i) in importantLinks"
+            :key="i"
+            :href="link.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ link.text }}
+          </a>
+        </v-row>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          Ecosystem
+        </h2>
+
+        <v-row justify="center">
+          <a
+            v-for="(eco, i) in ecosystem"
+            :key="i"
+            :href="eco.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ eco.text }}
+          </a>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import MyChart from './MyChart.vue';
+  export default {
+    name: 'HelloWorld',
 
-var spec_a = {
-  "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
-  "description": "asdfasdf",
-  data: {
-    values: [
-    {'symbol': 'MSFT', 'date': 'Jan 1 2000', 'price': 39.81},
-    {'symbol': 'MSFT', 'date': 'Feb 1 2000', 'price': 36.35},
-    {'symbol': 'MSFT', 'date': 'Mar 1 2000', 'price': 43.22},
-    {'symbol': 'MSFT', 'date': 'Apr 1 2000', 'price': 28.37},
-    {'symbol': 'MSFT', 'date': 'May 1 2000', 'price': 25.45},
-    {'symbol': 'MSFT', 'date': 'Jun 1 2000', 'price': 32.54},
-    {'symbol': 'MSFT', 'date': 'Jul 1 2000', 'price': 28.4},
-    {'symbol': 'MSFT', 'date': 'Aug 1 2000', 'price': 28.4},
-    {'symbol': 'MSFT', 'date': 'Sep 1 2000', 'price': 24.53},
-    {'symbol': 'MSFT', 'date': 'Oct 1 2000', 'price': 28.02},
-    ]
-  },
-  // "transform": [{"filter": "datum.symbol==='MSFT'"}],
-  "mark": "line",
-  "encoding": {
-    "x": {"field": "date", "type": "temporal"},
-    "y": {"field": "price", "type": "quantitative"}
-  }
-}
-
-export default {
-  name: 'HelloWorld',
-  components: {
-    MyChart
-  },
-  props: {
-    msg: String,
-  },
-  data: function() {
-    return {
-            first_spec: spec_a,
-            };
+    data: () => ({
+      ecosystem: [
+        {
+          text: 'vuetify-loader',
+          href: 'https://github.com/vuetifyjs/vuetify-loader',
         },
-  methods: {
-    getData(){
-      fetch('http://localhost:8000/data/yahoo?symbol=aapl', {mode:'cors'}).then(data => data.json()).then(data => {this.stuff = data;}).catch(err => console.log('Fetch Error :-S', err));
-    }
-  },
-  mounted() {
-    // this.stuff = this.getData();
-  },
-}
+        {
+          text: 'github',
+          href: 'https://github.com/vuetifyjs/vuetify',
+        },
+        {
+          text: 'awesome-vuetify',
+          href: 'https://github.com/vuetifyjs/awesome-vuetify',
+        },
+      ],
+      importantLinks: [
+        {
+          text: 'Documentation',
+          href: 'https://vuetifyjs.com',
+        },
+        {
+          text: 'Chat',
+          href: 'https://community.vuetifyjs.com',
+        },
+        {
+          text: 'Made with Vuetify',
+          href: 'https://madewithvuejs.com/vuetify',
+        },
+        {
+          text: 'Twitter',
+          href: 'https://twitter.com/vuetifyjs',
+        },
+        {
+          text: 'Articles',
+          href: 'https://medium.com/vuetify',
+        },
+      ],
+      whatsNext: [
+        {
+          text: 'Explore components',
+          href: 'https://vuetifyjs.com/components/api-explorer',
+        },
+        {
+          text: 'Select a layout',
+          href: 'https://vuetifyjs.com/layout/pre-defined',
+        },
+        {
+          text: 'Frequently Asked Questions',
+          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+        },
+      ],
+    }),
+  }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
