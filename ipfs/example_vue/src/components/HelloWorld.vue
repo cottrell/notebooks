4f81834d95b79feaf0e4d54b1eec407ad3f84458
David Cottrell <cottrell@users.noreply.github.com>
2020-12-30 21:46:02 +0000

    Found issue

--- ipfs/example_vue/src/components/HelloWorld.vue
+++ ipfs/example_vue/src/components/HelloWorld.vue
@@ -1,40 +1,36 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <button v-on:click="loadIPFS">CLICK</button>
   </div>
 </template>
 
 <script>
+
+const IPFS = require('ipfs-core')
+
+async function main() {
+    // const ipfs = await IPFS.create()
+    // const ipfs = await IPFS.create({rrrr: 'here'})
+    const ipfs = await IPFS.create({repo: String(Math.random() + Date.now()) })
+    // note the errors should go away with libp2p-websockets 0.15 release
+    const { cid } = await ipfs.add('Hello world')
+    console.info(cid)
+    // QmXXY5ZxbtuYj6DnfApLiGstzPN7fvSyigrRee3hDWPCaf
+    // process.exit()
+}
+
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  methods: {
+    loadIPFS: function() {
+        console.log('loadIPFS start')
+        main()
+        console.log('loadIPFS done')
+    },
   }
 }
 </script>
