7d2a8f843f520f4e3eddb7b107be37c3a0c86f79
David Cottrell <cottrell@users.noreply.github.com>
2021-01-02 20:39:50 +0000

    update

--- mkcert/generate_cert.sh
+++ mkcert/generate_cert.sh
@@ -3,5 +3,8 @@ type certutil || sudo apt install libnss3-tools
 localhostname=${1:-$(hostname)}
 echo mkcert localhost $localhostname.local 127.0.0.1 ::1 # Generate the certs
 mkcert localhost $localhostname.local 127.0.0.1 ::1 # Generate the certs
-echo run this to install:
+syncthing=$HOME/syncthing/cred/mkcert/$(hostname)
+mkdir -p $syncthing
+cp -v *pem $syncthing
+echo "run this on server and on hosts to install:"
 echo mkcert -install
