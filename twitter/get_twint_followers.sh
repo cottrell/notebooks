4e4f4e6097b700a52ca7629b71e3c46343df71e3
David Cottrell <cottrell@users.noreply.github.com>
2021-06-20 16:23:47 +0100

    update

---
+++ twitter/get_twint_followers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [[ $# -lt 1 ]]; then
+    echo usage: prog username [since]
+    exit 1
+fi
+since="${since:-2006-03-26}"
+username=$(echo $1 | tr '[:upper:]' '[:lower:]')
+filename=twint_"$username"_followers.csv
+cmd="twint --since $since --username $username --followers --csv --output $filename"
+# resume does not seem to work as expected, I think it goes backwards not forwards
+# if [[ -f $filename ]]; then
+#     # WARNING: fragile tab delimited file parsing relying on id being first entry in 2nd row
+#     id=$(head -2 $filename | tail -1 | cut -d'	' -f1)
+#     cmd="$cmd --resume $id"
+# fi
+echo $cmd
+eval $cmd
+
