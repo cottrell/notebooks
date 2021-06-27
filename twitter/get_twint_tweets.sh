0f1890e8d98a040e5a07095bc3dbde1c1fa0e81b
David Cottrell <cottrell@users.noreply.github.com>
2021-06-27 17:46:57 +0100

    Update twint script

--- twitter/get_twint_tweets.sh
+++ twitter/get_twint_tweets.sh
@@ -1,19 +1,16 @@
-#!/bin/bash
+#!/bin/bash -e
+# I think twitter started in 2006-03-26
 if [[ $# -lt 1 ]]; then
     echo usage: prog username [since]
     exit 1
 fi
-# since="${since:-2006-03-26}"
+username=$1
+shift
+since=$1
 since="${since:-2016-01-21}"
-username=$(echo $1 | tr '[:upper:]' '[:lower:]')
+username=$(echo $username | tr '[:upper:]' '[:lower:]')
 filename=twint_since_"$since"_$username.csv
 cmd="twint --since $since --username $username --csv --output $filename"
-# resume does not seem to work as expected, I think it goes backwards not forwards
-# if [[ -f $filename ]]; then
-#     # WARNING: fragile tab delimited file parsing relying on id being first entry in 2nd row
-#     id=$(head -2 $filename | tail -1 | cut -d'	' -f1)
-#     cmd="$cmd --resume $id"
-# fi
 echo $cmd
 eval $cmd
-
+gzip $filename
