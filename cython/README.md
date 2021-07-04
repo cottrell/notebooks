381843b98d80722f6ef7b8826e35b375ae4a67e0
David Cottrell <cottrell@users.noreply.github.com>
2021-07-04 21:25:01 +0100

    update

---
+++ cython/README.md
@@ -0,0 +1,3 @@
+https://cython.readthedocs.io/en/latest/src/userguide/wrapping_CPlusPlus.html
+
+TODO: some nunique thing with set
--- cython/do.py
+++
@@ -1,9 +0,0 @@
-cimport numpy as np
-
-cdef inline inc(int* arr, int i):
-    arr[i] += 1
-
-def test1(np.ndarray[np.int32_t] arr):
-    cdef int i
-    for i in xrange(len(arr)):
-        inc(<int*>arr.data, i)
