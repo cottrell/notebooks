8037f6f6aab51ef8ec6657e8b58d33cee96636e5
David Cottrell <cottrell@users.noreply.github.com>
2021-07-29 23:07:53 +0100

    update

--- jax/loop.py
+++ jax/loop.py
@@ -7,45 +7,87 @@ import pandas as pd
 import jax
 import jax.numpy as jnp
 
+alpha = 1e-2
+
 
 @jax.jit
-def loss(theta, y):
+def loss_fn_with_aux(theta, y):
     loss_reg = jnp.sum(theta ** 2)
     loss_data = jnp.sum((y - theta) ** 2)
     loss = loss_data + alpha * loss_reg
     return loss, dict(loss_reg=loss_reg, loss_data=loss_data)
 
 
+@jax.jit
+def loss_fn_no_aux(theta, y):
+    loss_reg = jnp.sum(theta ** 2)
+    loss_data = jnp.sum((y - theta) ** 2)
+    loss = loss_data + alpha * loss_reg
+    return loss
+
+
 m = 100
-key = jax.random.PRNGKey(0)
-theta = jax.random.normal(key, (m, 1))
 y = np.random.randn(m, 1).astype(np.float32)
-alpha = 1e-2
-step_size = 1e-2
 
 # test
-(value, aux), grad = jax.value_and_grad(loss, has_aux=True)(theta, y)
+(value, aux), grad = jax.value_and_grad(loss_fn_with_aux, has_aux=True)(theta, y)
 
 
-@jax.jit
-def update(theta, y):
-    (value, aux), grad = jax.value_and_grad(loss, has_aux=True)(theta, y)
-    # return value, [x - step_size * dx for x, dx in zip(params, grads)]
-    return aux, theta - step_size * grad
+def basic_loop(fig=1, step_size=1e-2, n_plot=100, n_steps=1000):
+
+    key = jax.random.PRNGKey(0)
+    theta = jax.random.normal(key, (m, 1))
+
+    @jax.jit
+    def step(theta, y):
+        (value, aux), grad = jax.value_and_grad(loss_fn_with_aux, has_aux=True)(theta, y)
+        # return value, [x - step_size * dx for x, dx in zip(params, grads)]
+        return aux, theta - step_size * grad
+
+    ldata = list()
+    plot_period = n_steps // n_plot
+    for i in range(n_steps):
+        aux, theta = step(theta, y)
+        if i % plot_period == 0:
+            aux = {k: v.item() for k, v in aux.items()}
+            ldata.append(aux)
+
+    ldata = pd.DataFrame(ldata)
+    fig = plt.figure(fig)
+    fig.clf()
+    ax = fig.subplots(1)
+    ldata.plot(ax=ax)
+    ax.set_title('basic_loop')
 
 
-ldata = list()
-n_plot = 100
-n_steps = 1000
-plot_period = n_steps // n_plot
-for i in range(n_steps):
-    aux, theta = update(theta, y)
-    if i % plot_period == 0:
-        aux = {k: v.item() for k, v in aux.items()}
-        ldata.append(aux)
+def adam_loop(fig=2, learning_rate=1e-5, n_plot=10000, n_steps=10000):
 
-ldata = pd.DataFrame(ldata)
-fig = plt.figure(1)
-fig.clf()
-ax = fig.subplots(1)
-ldata.plot(ax=ax)
+    key = jax.random.PRNGKey(0)
+    theta = jax.random.normal(key, (m, 1))
+    opt_init, opt_update, opt_get_params = jax.experimental.optimizers.sgd(learning_rate)
+    opt_state = opt_init(theta)
+
+    @jax.jit
+    def step(step, opt_state):
+        (value, aux), grads = jax.value_and_grad(loss_fn_with_aux, has_aux=True)(opt_get_params(opt_state), y)
+        # value, grads = jax.value_and_grad(loss_fn_no_aux, has_aux=False)(opt_get_params(opt_state), y)
+        opt_state = opt_update(step, grads, opt_state)
+        return aux, opt_state
+
+    for i in range(n_steps):
+        value, opt_state = step(i, opt_state)
+
+    ldata = list()
+    plot_period = n_steps // n_plot
+    for i in range(n_steps):
+        aux, opt_state = step(i, opt_state)
+        if i % plot_period == 0:
+            aux = {k: v.item() for k, v in aux.items()}
+            ldata.append(aux)
+
+    ldata = pd.DataFrame(ldata)
+    fig = plt.figure(fig)
+    fig.clf()
+    ax = fig.subplots(1)
+    ldata.plot(ax=ax)
+    ax.set_title('adam')
