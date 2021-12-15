a27f6500b7756eee5ee06ccd4670b8956837500f
David Cottrell <cottrell@users.noreply.github.com>
2021-12-15 17:41:11 +0000

    update

--- web/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-# https://johnpapa.net/node-and-npm-without-sudo/
-# manual: Install Node.js from https://nodejs.org/en/download/
-sudo npm install npm -g
-mkdir -p ~/.npm-packages
-npm config set prefix ~/.npm-packages
-npm install @vue/cli @vue/cli-service -g
