5a003cb0750f83c3634dbfe5ab8de5082c81ddf1
David Cottrell <cottrell@users.noreply.github.com>
2022-08-08 12:39:41 +0100

    update

--- cython/embed/example.py
+++ cython/embed/example.py
@@ -1,10 +1,12 @@
+#cython: language_level=3
+# https://github.com/astropy/astropy/pull/8106/commits/73fc5a4537af3fc79165b400434a8caaf55b1645
 import numpy as np
-import csv
+import pandas as pd
 
 def main():
-    x = np.random.randn(10000, 3)
-    for i in range(x.shape[0]):
-        print(x[i])
+    x = np.random.randn(10, 3)
+    df = pd.DataFrame(x)
+    print(df)
     print('look at me')
 
 if __name__ == '__main__':
