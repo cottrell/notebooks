6f71264eb794e1cde19f31af23b2408dee44f3e2
David Cottrell <cottrell@users.noreply.github.com>
2022-09-20 11:46:49 +0100

    update

--- cython/embed/example.py
+++ cython/embed/example.py
@@ -1,12 +1,13 @@
 #cython: language_level=3
 # https://github.com/astropy/astropy/pull/8106/commits/73fc5a4537af3fc79165b400434a8caaf55b1645
 import numpy as np
-import pandas as pd
+# import pandas as pd
 
 def main():
     x = np.random.randn(10, 3)
-    df = pd.DataFrame(x)
-    print(df)
+    print(x)
+    # df = pd.DataFrame(x)
+    # print(df)
     print('look at me')
 
 if __name__ == '__main__':
