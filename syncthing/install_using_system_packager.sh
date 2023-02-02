3c7a358fe079288cc202e4437c87f2f3d1df84de
David Cottrell <cottrell@users.noreply.github.com>
2023-02-02 12:55:58 +0000

    update

--- syncthing/install_using_system_packager.sh
+++ syncthing/install_using_system_packager.sh
@@ -2,9 +2,9 @@
 if [[ $(uname) = "Linux" ]]; then
     echo installing for Ubuntu
     # Add the release PGP keys:
-    curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
+    sudo curl -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
     # Add the "stable" channel to your APT sources:
-    echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+    echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
     # Update and install syncthing:
     sudo apt-get update
     sudo apt-get install syncthing
