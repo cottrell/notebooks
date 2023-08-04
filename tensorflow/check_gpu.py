92882302d4bd38af8eb469880b19c6bc7d522222
David Cottrell <cottrell@users.noreply.github.com>
2023-08-04 10:06:00 +0100

    update

--- tensorflow/check_gpu.py
+++ tensorflow/check_gpu.py
@@ -3,6 +3,13 @@ import tensorflow as tf
 
 devices = tf.config.list_physical_devices('GPU')
 
+import os
+
+print(f'NOTE: LD_LIBRARY_PATH={os.environ["LD_LIBRARY_PATH"]} ... 2023-08-04 you needed this as in the comment in this file')
+# NOTE: 2023-08-04
+# ~/anaconda3/envs/3.11.0/lib/:~/anaconda3/envs/3.11.0/lib/python3.11/site-packages/nvidia/cudnn/lib:~/anaconda3/envs/3.11.0/lib/:~/anaconda3/envs/3.11.0/lib/python3.11/site-packages/nvidia/cudnn/lib:
+# ... see bashrc and bash profile and log notes
+
 if devices:
     print('found GPU devices:')
     print(devices)
