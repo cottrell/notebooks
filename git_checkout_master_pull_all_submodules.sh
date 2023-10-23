5b8b0de1e78d69e8bf130deb2dd6836c0769f57e
David Cottrell <cottrell@users.noreply.github.com>
2023-10-23 11:43:30 +0100

    Fix.

--- git_checkout_master_pull_all_submodules.sh
+++ git_checkout_master_pull_all_submodules.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -e
 git pull # you forget this
+# ugh
+# master=$(git rev-parse --verify master 2>/dev/null 1>&2 && echo master || echo main)
+# git submodule foreach --recursive 'git checkout master; git pull origin master:master; :'
 git submodule foreach --recursive 'git checkout master; git pull origin master:master; :'
