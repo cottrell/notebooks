bfd519a1d95eb57d02af2ccb4fe9553cad59205d
David Cottrell <cottrell@users.noreply.github.com>
2023-11-08 21:51:41 +0000

    update from bleepblop

--- python/pytorch.sh
+++ python/pytorch.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
-MY_CONDA_ENV=A
+#!/bin/bash -e
+MY_CONDA_ENV=pytorch
 # PYTHON_ENV=3
 PYTHON_ENV=3.11
-conda create -c conda-forge -y -n $MY_CONDA_ENV python=PYTHON_ENV
-pip install -U pip
-pip install -U gpytorch
+conda create -c conda-forge -y -n $MY_CONDA_ENV python=$PYTHON_ENV
+echo conda activate $MY_CONDA_ENV
+echo pip install -U pip
+echo pip install -U gpytorch
--- torch/check_gpu.py
+++ torch/check_gpu.py
@@ -3,12 +3,12 @@ import torch
 torch.cuda.current_device()
 torch.cuda.device_count()
 torch.cuda.get_device_name(0)
-# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# print('Using device:', device)
-# print()
-# 
-# if device.type == 'cuda':
-#     print(torch.cuda.get_device_name(0))
-#     print('Memory Usage:')
-#     print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
-#     print('Cached:   ', round(torch.cuda.memory_cached(0)/1024**3,1), 'GB')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device:', device)
+print()
+
+if device.type == 'cuda':
+    print(torch.cuda.get_device_name(0))
+    print('Memory Usage:')
+    print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
+    print('Cached:   ', round(torch.cuda.memory_cached(0)/1024**3,1), 'GB')
