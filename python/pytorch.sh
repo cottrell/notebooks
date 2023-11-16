84f4389700dc0a971a60e04f50bdbd06abcb8541
David Cottrell <cottrell@users.noreply.github.com>
2023-11-16 21:19:35 +0000

    update from bleepblop

--- python/pytorch.sh
+++ python/pytorch.sh
@@ -5,4 +5,5 @@ PYTHON_ENV=3.11
 conda create -c conda-forge -y -n $MY_CONDA_ENV python=$PYTHON_ENV
 echo conda activate $MY_CONDA_ENV
 echo pip install -U pip
-echo pip install -U gpytorch
+echo pip install -r ./requirements_pytorch.txt
+echo ./local_setup.sh
