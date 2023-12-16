a056d4f9fd0ae3b403d9448ec5af83518c276747
David Cottrell <cottrell@users.noreply.github.com>
2023-12-16 20:29:40 +0000

    update from bleepblop

--- tensorflow/check_gpu.py
+++ tensorflow/check_gpu.py
@@ -5,7 +5,9 @@ devices = tf.config.list_physical_devices('GPU')
 
 import os
 
-print(f'NOTE: LD_LIBRARY_PATH={os.environ["LD_LIBRARY_PATH"]} ... 2023-08-04 you needed this as in the comment in this file')
+# NOTE: 2023-12-16 you no longer needed.
+#
+# print(f'NOTE: LD_LIBRARY_PATH={os.environ["LD_LIBRARY_PATH"]} ... 2023-08-04 you needed this as in the comment in this file')
 # NOTE: 2023-08-04
 # ~/anaconda3/envs/3.11.0/lib/:~/anaconda3/envs/3.11.0/lib/python3.11/site-packages/nvidia/cudnn/lib:~/anaconda3/envs/3.11.0/lib/:~/anaconda3/envs/3.11.0/lib/python3.11/site-packages/nvidia/cudnn/lib:
 # ... see bashrc and bash profile and log notes
