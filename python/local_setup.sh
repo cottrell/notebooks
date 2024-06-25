cd0e19a6360c77878d114c17c1b54ba728db5995
David Cottrell <cottrell@users.noreply.github.com>
2024-06-25 13:14:44 +0100

    update from bleepblop

--- python/local_setup.sh
+++ python/local_setup.sh
@@ -7,3 +7,4 @@ cd $HOME/projects/notebooks/my-gym && $CMD
 cd $HOME/projects/notebooks/my-gym/my/gym/usr/cottrell/synthetic_data_generators && $CMD
 cd $HOME/projects/notebooks/my-gym/my/gym/usr/cottrell/equinox_utils && $CMD
 cd $HOME/projects/notebooks/my-gym/my/gym/usr/cottrell/scraper_utils && $CMD
+cd $HOME/projects/notebooks/my-gym/toplevelrepo/our/our/extractors/xml_iterator && make build && $CMD
