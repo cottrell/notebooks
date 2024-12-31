088898a167e6cbed966dccbdb72c9aae72eb3e13
David Cottrell <cottrell@users.noreply.github.com>
2024-12-31 14:55:32 +0000

    update from bleepblop

--- jax/check_gpu.py
+++ jax/check_gpu.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 def check_gpu():
-    from jax.lib import xla_bridge
+    from jax.extend.backend import get_backend
     import jax.numpy as jnp
     from jax import random
     key = random.PRNGKey(0)
@@ -9,7 +9,7 @@ def check_gpu():
     x = random.normal(key, (size, size), dtype=jnp.float32)
     jnp.dot(x, x.T).block_until_ready()  # runs on the GPU
 
-    return xla_bridge.get_backend().platform
+    return get_backend().platform
 
 
 if __name__ == '__main__':
