ceebc550026ff06723b1b6b1c28beb8905a94ef3
David Cottrell <cottrell@users.noreply.github.com>
2025-06-10 14:41:57 +0100

    update from bleepblop

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -9,6 +9,9 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 # nvm install 22  # for example, claude doesn't work on this one 2025-06-10
 nvm install 20
 
+# maybe do this
+nvm alias default 20
+
 # Verify the Node.js version:
 node -v # Should print "v22.16.0".
 nvm current # Should print "v22.16.0".
