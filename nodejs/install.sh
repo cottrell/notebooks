719bb6e94b783218e408694de654726073ff9bee
David Cottrell <cottrell@users.noreply.github.com>
2025-06-10 12:45:15 +0100

    node note.

---
+++ nodejs/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+# in lieu of restarting the shell
+\. "$HOME/.nvm/nvm.sh"
+
+# Download and install Node.js:
+# nvm install 22  # for example, claude doesn't work on this one 2025-06-10
+nvm install 20
+
+# Verify the Node.js version:
+node -v # Should print "v22.16.0".
+nvm current # Should print "v22.16.0".
+
+# Verify npm version:
+npm -v # Should print "10.9.2".
+
