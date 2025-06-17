5a6a701c1e78ee999ea4637bb1e9e8f9689245ef
David Cottrell <cottrell@users.noreply.github.com>
2025-06-17 12:05:34 +0100

    install both.

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -6,7 +6,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 \. "$HOME/.nvm/nvm.sh"
 
 # Download and install Node.js:
-# nvm install 22  # for example, claude doesn't work on this one 2025-06-10
+nvm install 22  # for example, claude doesn't work on this one 2025-06-10
 nvm install 20
 
 # maybe do this
