176f7a37303eef4a57b8c97b8d0fdabfc21d2780
David Cottrell <cottrell@users.noreply.github.com>
2025-12-12 07:25:30 +0000

    update from bleepblop

--- ai.sh
+++ ai.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+type npm | cd npm && make install && cd -
 type bun | cd bun && make install && cd -
+
+# good idea? or bad?
+cd npm && make update && cd -
+cd bun && make update && cd -
+
 cd gemini && make install && cd -
 cd claude && make install && cd -
 cd codex && make install && cd -
