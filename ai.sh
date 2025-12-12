4951bfee3ec4ae6094a770877270216d00465155
David Cottrell <cottrell@users.noreply.github.com>
2025-12-12 07:23:01 +0000

    Avoid prompt.

--- ai.sh
+++ ai.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+type bun | cd bun && make install && cd -
+cd gemini && make install && cd -
+cd claude && make install && cd -
+cd codex && make install && cd -
+cd mistral && make install && cd -
