ccbf31095d63cbeaa083d02b840ce9228f6cf9ce
David Cottrell <cottrell@users.noreply.github.com>
2025-12-12 07:51:05 +0000

    update from bleepblop

--- ai.sh
+++ ai.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-if type npm >/dev/null 2>&1; then (cd npm && make install); fi
-if type bun >/dev/null 2>&1; then (cd bun && make install); fi
+if ! type node >/dev/null 2>&1; then (cd nodejs && make install); else (cd nodejs && make update); fi
+if ! type bun >/dev/null 2>&1; then (cd bun && make install); else (cd bun && make update); fi
 
 # good idea? or bad?
-(cd npm && make update)
-(cd bun && make update)
-
-(cd gemini && make install)
-(cd claude && make install)
-(cd codex && make install)
-(cd mistral && make install)
+if ! type gemini >/dev/null 2>&1; then (cd gemini && make install); else (cd gemini && make update); fi
+if ! type claude >/dev/null 2>&1; then (cd claude && make install); else (cd claude && make update); fi
+if ! type codex >/dev/null 2>&1; then (cd codex && make install); else (cd codex && make update); fi
+if ! type mistral >/dev/null 2>&1; then (cd mistral && make install); else (cd mistral && make update); fi
